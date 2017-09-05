e5faf7d99ac085693f61e87f324623b0efa34bda
obarranco <obarranco@soltecsis.com>
2017-09-05 11:41:51 +0200

    undefined control

--- routes/policy_r__ipobjs.js
+++ routes/policy_r__ipobjs.js
@@ -3,87 +3,89 @@ var router = express.Router();
 var Policy_r__ipobjModel = require('../models/policy_r__ipobj');
 
 /**
-* Property Logger to manage App logs
-*
-* @property logger
-* @type log4js/app
-* 
-*/
+ * Property Logger to manage App logs
+ *
+ * @property logger
+ * @type log4js/app
+ * 
+ */
 var logger = require('log4js').getLogger("app");
 
 
-router.param('rule', function(req, res, next, param) {
-    if (param===undefined || param==='' || isNaN(param))
-        res.status(404).json( {"msg": "param rule Error"});
+router.param('rule', function (req, res, next, param) {
+    if (param === undefined || param === '' || isNaN(param))
+        res.status(404).json({"msg": "param rule Error"});
     else
-        next(); 
+        next();
 });
-router.param('ipobj', function(req, res, next, param) {
-    if (param===undefined || param==='' || isNaN(param))    
-        req.ipobj=0;
-            
-        next(); 
-});
-router.param('ipobj_g', function(req, res, next, param) {
-    if (param===undefined || param==='' || isNaN(param))    {
-        req.ipobj_g=0;
+router.param('ipobj', function (req, res, next, param) {
+    if (param === undefined || param === '' || isNaN(param)) {
+        logger.error("DETECTED UNDEFINED: ipobj");
+        req.params.ipobj = 0;
     }
-    next(); 
+    next();
 });
-router.param('interface', function(req, res, next, param) {
-    if (param===undefined || param==='' || isNaN(param))    {
-        req.interface=0;
+router.param('ipobj_g', function (req, res, next, param) {
+    if (param === undefined || param === '' || isNaN(param)) {
+        logger.error("DETECTED UNDEFINED: ipobj_g");
+        req.params.ipobj_g = 0;
     }
-    next(); 
+    next();
 });
-router.param('position', function(req, res, next, param) {
-    if (param===undefined || param==='' || isNaN(param))    {
-        res.status(404).json( {"msg": "param position Error"});
+router.param('interface', function (req, res, next, param) {
+    if (param === undefined || param === '' || isNaN(param)) {
+        logger.error("DETECTED UNDEFINED: interface");
+        req.params.interface = 0;
     }
+    next();
+});
+router.param('position', function (req, res, next, param) {
+    if (param === undefined || param === '' || isNaN(param)) {
+        res.param.status(404).json({"msg": "param position Error"});
+    } else
+        next();
+});
+router.param('position_order', function (req, res, next, param) {
+    if (param === undefined || param === '' || isNaN(param)) {
+        req.params.position = 1;
+    }
+    next();
+});
+router.param('new_rule', function (req, res, next, param) {
+    if (param === undefined || param === '' || isNaN(param))
+        res.status(404).json({"msg": "param new_rule Error"});
     else
-        next(); 
+        next();
 });
-router.param('position_order', function(req, res, next, param) {
-    if (param===undefined || param==='' || isNaN(param))    {
-        req.position=1;
+router.param('new_position', function (req, res, next, param) {
+    if (param === undefined || param === '' || isNaN(param)) {
+        res.status(404).json({"msg": "param new_position Error"});
+    } else
+        next();
+});
+router.param('new_order', function (req, res, next, param) {
+    if (param === undefined || param === '' || isNaN(param)) {
+        req.params.new_order = 1;
     }
-    next(); 
+    next();
 });
-router.param('new_rule', function(req, res, next, param) {
-    if (param===undefined || param==='' || isNaN(param))
-        res.status(404).json( {"msg": "param new_rule Error"});
-    else
-        next(); 
-});
-router.param('new_position', function(req, res, next, param) {
-    if (param===undefined || param==='' || isNaN(param))    {
-        res.status(404).json( {"msg": "param new_position Error"});
+router.param('negate', function (req, res, next, param) {
+    if (param === undefined || param === '' || isNaN(param)) {
+        logger.error("DETECTED UNDEFINED: negate");
+        req.params.negate = 0;
     }
-    else
-        next(); 
-});
-router.param('new_order', function(req, res, next, param) {
-    if (param===undefined || param==='' || isNaN(param))    {
-        req.new_order=1;
-    }
-    next(); 
-});
-router.param('negate', function(req, res, next, param) {
-    if (param===undefined || param==='' || isNaN(param))    {
-        req.negate=0;
-    }
-    next(); 
+    next();
 });
 
 
-function checkPostParameters(obj){
+function checkPostParameters(obj) {
     logger.debug(obj);
-    
-    for(var propt in obj){
+
+    for (var propt in obj) {
         logger.debug(propt + ': ' + obj[propt]);
-        if (obj[propt]===undefined){
+        if (obj[propt] === undefined) {
             logger.debug("PARAMETRO UNDEFINED: " + propt);
-            obj[propt]=0;
+            obj[propt] = 0;
         }
     }
     return obj;
@@ -101,21 +103,21 @@ router.get('/policy-r__ipobj', function (req, res)
 router.get('/:firewall/:rule', function (req, res)
 {
     var rule = req.params.rule;
-    
-    Policy_r__ipobjModel.getPolicy_r__ipobjs(rule,function (error, data)
+
+    Policy_r__ipobjModel.getPolicy_r__ipobjs(rule, function (error, data)
     {
         //If exists policy_r__ipobj get data
-        if (typeof data !== 'undefined' && data.length>0)
-        {            
-            res.status(200).json( {"data": data});
+        if (typeof data !== 'undefined' && data.length > 0)
+        {
+            res.status(200).json({"data": data});
         }
         //Get Error
         else
         {
-            res.status(404).json( {"msg": "notExist"});
+            res.status(404).json({"msg": "notExist"});
         }
     });
-    
+
 });
 
 /* Get all policy_r__ipobjs by rule and posicion*/
@@ -124,21 +126,21 @@ router.get('/:firewall/:rule/:position', function (req, res)
 {
     var rule = req.params.rule;
     var position = req.params.position;
-    
-    Policy_r__ipobjModel.getPolicy_r__ipobjs_position(rule,position,function (error, data)
+
+    Policy_r__ipobjModel.getPolicy_r__ipobjs_position(rule, position, function (error, data)
     {
         //If exists policy_r__ipobj get data
-        if (typeof data !== 'undefined' && data.length>0)
+        if (typeof data !== 'undefined' && data.length > 0)
         {
-            res.status(200).json( {"data": data});
+            res.status(200).json({"data": data});
         }
         //Get Error
         else
         {
-            res.status(404).json( {"msg": "notExist"});
+            res.status(404).json({"msg": "notExist"});
         }
     });
-    
+
 });
 
 /* Get all policy_r__ipobjs by rule and posicion with IPOBJ DATA*/
@@ -147,21 +149,21 @@ router.get('/data/:firewall/:rule/:position', function (req, res)
 {
     var rule = req.params.rule;
     var position = req.params.position;
-    
-    Policy_r__ipobjModel.getPolicy_r__ipobjs_position_data(rule,position,function (error, data)
+
+    Policy_r__ipobjModel.getPolicy_r__ipobjs_position_data(rule, position, function (error, data)
     {
         //If exists policy_r__ipobj get data
-        if (typeof data !== 'undefined' && data.length>0)
+        if (typeof data !== 'undefined' && data.length > 0)
         {
-            res.status(200).json( {"data": data});
+            res.status(200).json({"data": data});
         }
         //Get Error
         else
         {
-            res.status(404).json( {"msg": "notExist"});
+            res.status(404).json({"msg": "notExist"});
         }
     });
-    
+
 });
 
 
@@ -174,19 +176,19 @@ router.get('/:firewall/:rule/:ipobj/:ipobj_g/:interface/:position', function (re
     var ipobj_g = req.params.ipobj_g;
     var interface = req.params.interface;
     var position = req.params.position;
-    
-    Policy_r__ipobjModel.getPolicy_r__ipobj(rule,ipobj,ipobj_g,interface,position,function (error, data)
+
+    Policy_r__ipobjModel.getPolicy_r__ipobj(rule, ipobj, ipobj_g, interface, position, function (error, data)
     {
         //If exists policy_r__ipobj get data
-        if (typeof data !== 'undefined' && data.length>0)
+        if (typeof data !== 'undefined' && data.length > 0)
         {
 
-            res.status(200).json( {"data": data});
+            res.status(200).json({"data": data});
         }
         //Get Error
         else
         {
-            res.status(404).json( {"msg": "notExist"});
+            res.status(404).json({"msg": "notExist"});
         }
     });
 });
@@ -202,24 +204,24 @@ router.post("/policy-r__ipobj", function (req, res)
         ipobj_g: req.body.ipobj_g,
         interface: req.body.interface,
         position: req.body.position,
-        position_order: req.body.position_order        
-        
+        position_order: req.body.position_order
+
     };
-    
-    
-    policy_r__ipobjData=checkPostParameters(policy_r__ipobjData);
-        
-    
-    Policy_r__ipobjModel.insertPolicy_r__ipobj(policy_r__ipobjData,0, function (error, data)
+
+
+    policy_r__ipobjData = checkPostParameters(policy_r__ipobjData);
+
+
+    Policy_r__ipobjModel.insertPolicy_r__ipobj(policy_r__ipobjData, 0, function (error, data)
     {
         //If saved policy_r__ipobj Get data
         if (data && data.msg)
         {
             //res.redirect("/policy-r__ipobjs/policy-r__ipobj/" + data.insertId);
-            res.status(200).json( data.msg);
+            res.status(200).json(data.msg);
         } else
         {
-            res.status(500).json( {"error": error});
+            res.status(500).json({"error": error});
         }
     });
 });
@@ -233,8 +235,8 @@ router.put('/policy-r__ipobj', function (req, res)
     var interface = req.body.get_interface;
     var position = req.body.get_position;
     var position_order = req.body.get_position_order;
-        
-    
+
+
     //Save new data into object
     var policy_r__ipobjData = {
         rule: req.body.rule,
@@ -243,22 +245,22 @@ router.put('/policy-r__ipobj', function (req, res)
         interface: req.body.interface,
         position: req.body.position,
         position_order: req.body.position_order
-       
+
     };
-    
-    policy_r__ipobjData=checkPostParameters(policy_r__ipobjData);
-      
-    
-    Policy_r__ipobjModel.updatePolicy_r__ipobj(rule,ipobj,ipobj_g,interface,position, position_order,policy_r__ipobjData, function (error, data)
+
+    policy_r__ipobjData = checkPostParameters(policy_r__ipobjData);
+
+
+    Policy_r__ipobjModel.updatePolicy_r__ipobj(rule, ipobj, ipobj_g, interface, position, position_order, policy_r__ipobjData, function (error, data)
     {
         //If saved policy_r__ipobj saved ok, get data
         if (data && data.msg)
         {
             //res.redirect("/policy-r__ipobjs/policy-r__ipobj/" + req.param('id'));
-            res.status(200).json( data.msg);
+            res.status(200).json(data.msg);
         } else
         {
-            res.status(500).json( {"error": error});
+            res.status(500).json({"error": error});
         }
     });
 });
@@ -275,18 +277,18 @@ router.put('/policy-r__ipobj/:firewall/:rule/:ipobj/:ipobj_g/:interface/:positio
     var new_rule = req.params.new_rule;
     var new_position = req.params.new_position;
     var new_order = req.params.new_order;
+
     
-    
-    Policy_r__ipobjModel.updatePolicy_r__ipobj_position(rule,ipobj,ipobj_g,interface,position,position_order,new_rule,new_position,new_order, function (error, data)
+    Policy_r__ipobjModel.updatePolicy_r__ipobj_position(rule, ipobj, ipobj_g, interface, position, position_order, new_rule, new_position, new_order, function (error, data)
     {
         //If saved policy_r__ipobj saved ok, get data
         if (data && data.msg)
         {
             //res.redirect("/policy-r__ipobjs/policy-r__ipobj/" + req.param('id'));
-            res.status(200).json( data.msg);
+            res.status(200).json(data.msg);
         } else
         {
-            res.status(500).json( {"msg": error});
+            res.status(500).json({"msg": error});
         }
     });
 });
@@ -298,18 +300,18 @@ router.put('/policy-r__ipobj/:firewall/:rule/:position/:negate', function (req, 
     var rule = req.params.rule;
     var position = req.params.position;
     var negate = req.params.negate;
-    
 
-    Policy_r__ipobjModel.updatePolicy_r__ipobj_negate(rule,position,negate, function (error, data)
+
+    Policy_r__ipobjModel.updatePolicy_r__ipobj_negate(rule, position, negate, function (error, data)
     {
         //If saved policy_r__ipobj saved ok, get data
         if (data && data.msg)
         {
             //res.redirect("/policy-r__ipobjs/policy-r__ipobj/" + req.param('id'));
-            res.status(200).json( data.msg);
+            res.status(200).json(data.msg);
         } else
         {
-            res.status(500).json( {"msg": error});
+            res.status(500).json({"msg": error});
         }
     });
 });
@@ -324,19 +326,19 @@ router.put('/policy-r__ipobj/:rule/:ipobj/:ipobj_g/:position/:position_order/:ne
     var position = req.params.position;
     var position_order = req.params.position_order;
     var new_order = req.params.new_order;
-    
-    
 
-    Policy_r__ipobjModel.updatePolicy_r__ipobj_position_order(rule,ipobj,ipobj_g,interface,position,position_order,new_order, function (error, data)
+
+
+    Policy_r__ipobjModel.updatePolicy_r__ipobj_position_order(rule, ipobj, ipobj_g, interface, position, position_order, new_order, function (error, data)
     {
         //If saved policy_r__ipobj saved ok, get data
         if (data && data.msg)
         {
             //res.redirect("/policy-r__ipobjs/policy-r__ipobj/" + req.param('id'));
-            res.status(200).json( data.msg);
+            res.status(200).json(data.msg);
         } else
         {
-            res.status(500).json( {"msg": error});
+            res.status(500).json({"msg": error});
         }
     });
 });
@@ -354,16 +356,16 @@ router.delete("/policy-r__ipobj/", function (req, res)
     var interface = req.params.interface;
     var position = req.params.position;
     var position_order = req.params.position_order;
-    
-    Policy_r__ipobjModel.deletePolicy_r__ipobj(rule,ipobj,ipobj_g,interface, position, position_order, function (error, data)
+
+    Policy_r__ipobjModel.deletePolicy_r__ipobj(rule, ipobj, ipobj_g, interface, position, position_order, function (error, data)
     {
         if (data && data.msg === "deleted" || data.msg === "notExist")
         {
             //res.redirect("/policy-r__ipobjs/");
-            res.status(200).json( data.msg);
+            res.status(200).json(data.msg);
         } else
         {
-            res.status(500).json( {"msg": error});
+            res.status(500).json({"msg": error});
         }
     });
 });
