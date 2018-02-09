c67230b66a8eef11f6aac341f73fca7fc4f9e382
obarranco <obarranco@soltecsis.com>
2018-02-09 17:35:51 +0100

    Modifications to add Streamings process to compile rules

--- routes/stream/stream.js
+++ routes/stream/stream.js
@@ -88,6 +88,76 @@ router.get('/update-stream/compile/:iduser/:fwcloud', function (req, res) {
     }
 });
 
+router.get('/update-stream/compile', function (req, res) {
+
+
+    try {
+        // let request last as long as possible
+        req.socket.setTimeout(99999999);
+        var accessData = {sessionID: req.sessionID, iduser: "1", fwcloud: "1"};
+
+        
+        
+        var messageCount = 0;
+        var subscriber = redis.createClient();
+        
+        var channel= StreamModel.getTagPublishCompile(accessData);
+
+        logger.debug("REQUESTING SUBSCRIPTON STEAMING COMPILE DATA. CHANNEL : " + channel);
+        
+        
+        subscriber.subscribe(channel);
+
+        // In case we encounter an error...print it out to the console
+        subscriber.on("error", function (err) {
+            logger.debug("Redis Error: " + err);
+        });
+
+        // When we receive a message from the redis connection
+        subscriber.on("message", function (channel, message) {
+            messageCount++; // Increment our message count
+
+            logger.debug("CHANNEL: " + channel);
+            logger.debug("RECEIVING STREAMING COMPILE DATA: " + messageCount + "  MSG: " + message);
+            
+
+            //res.write('id: ' + messageCount + '\n');
+            //res.write("data: " + message + '\n\n'); // Note the extra newline
+
+            // Flush out line by line.
+            var str = message;
+            var lines = str.split("\n");
+            for (var i in lines) {
+                if (i == lines.length - 1) {
+                    str = lines[i];
+                } else {
+                    // Note: The double-newline is *required*
+                    res.write('data: ' + lines[i] + "\n\n");
+                }
+            }
+        });
+
+        //send headers for event-stream connection
+        res.writeHead(200, {
+            'Content-Type': 'text/event-stream',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive'
+        });
+        res.write('\n');
+
+        // The 'close' event is fired when a user closes their browser window.
+        // In that situation we want to make sure our redis channel subscription
+        // is properly shut down to prevent memory leaks...and incorrect subscriber
+        // counts to the channel.
+        req.on("close", function () {
+            subscriber.unsubscribe();
+            subscriber.quit();
+        });
+    } catch (err) {
+        res.write('data: ' + "\n\n");
+    }
+});
+
 router.get('/fire-event/:event_name', function (req, res) {
     var event_name = req.params.event_name;
 
