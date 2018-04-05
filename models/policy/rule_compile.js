0d31dee40d25695782f1d825b797d1d946025b59
Carles Xavier Munyoz Baldó <carles@MacBook-Pro-de-Carles-3.local>
2018-04-06 00:48:19 +0200

    New function getPolicy_rs_type_full for collect compilation data.

--- models/policy/rule_compile.js
+++ models/policy/rule_compile.js
@@ -164,15 +164,15 @@ RuleCompileModel.pre_compile = (data) => {
 	// WARNING: The order of creation of the arrays is important for optimization!!!!
 	// The positions first in the array will be used first in the conditions.
 	// INTERFACE IN / OUT
-	if (items=RuleCompileModel.pre_compile_if(((policy_type===POLICY_TYPE_OUTPUT || policy_type===POLICY_TYPE_SNAT) ? "-o " : "-i "), data[0].positions[0].ipobjs)) position_items.push(items);
+	if (items=RuleCompileModel.pre_compile_if(((policy_type===POLICY_TYPE_OUTPUT || policy_type===POLICY_TYPE_SNAT) ? "-o " : "-i "), data[0].positions[0].position_objs)) position_items.push(items);
 	// INTERFACE OUT
-	if (policy_type===POLICY_TYPE_FORWARD && (items=RuleCompileModel.pre_compile_if("-o ", data[0].positions[1].ipobjs))) position_items.push(items);
+	if (policy_type===POLICY_TYPE_FORWARD && (items=RuleCompileModel.pre_compile_if("-o ", data[0].positions[1].position_objs))) position_items.push(items);
 	// SERVICE
-	if (items=RuleCompileModel.pre_compile_svc(":",data[0].positions[svc_position].ipobjs)) position_items.push(items);
+	if (items=RuleCompileModel.pre_compile_svc(":",data[0].positions[svc_position].position_objs)) position_items.push(items);
 	// SOURCE
-	if (items=RuleCompileModel.pre_compile_sd("-s ", data[0].positions[src_position].ipobjs)) position_items.push(items);
+	if (items=RuleCompileModel.pre_compile_sd("-s ", data[0].positions[src_position].position_objs)) position_items.push(items);
 	// DESTINATION
-	if (items=RuleCompileModel.pre_compile_sd("-d ", data[0].positions[dst_position].ipobjs)) position_items.push(items);
+	if (items=RuleCompileModel.pre_compile_sd("-d ", data[0].positions[dst_position].position_objs)) position_items.push(items);
 
 	// Order the resulting array by number of strings into each array.
 	if (position_items.length < 2) // Don't need ordering.
@@ -263,7 +263,7 @@ RuleCompileModel.rule_compile = (cloud, fw, type, rule, callback) => {
 		if (policy_type === 4) { // SNAT
 			table = "-t nat";
 			cs += table+" -A POSTROUTING ";
-			if (!(action=RuleCompileModel.nat_action(policy_type,data[0].positions[4].ipobjs,data[0].positions[5].ipobjs))) {
+			if (!(action=RuleCompileModel.nat_action(policy_type,data[0].positions[4].position_objs,data[0].positions[5].position_objs))) {
 				callback({"Msg": "Invalid NAT positions."},null);
 				return;
 			}
@@ -271,15 +271,15 @@ RuleCompileModel.rule_compile = (cloud, fw, type, rule, callback) => {
 		else if (policy_type === 5) { // DNAT
 			table = "-t nat";
 			cs += table+" -A PREROUTING ";
-			if (!(action=RuleCompileModel.nat_action(policy_type,data[0].positions[4].ipobjs,data[0].positions[5].ipobjs))) {
+			if (!(action=RuleCompileModel.nat_action(policy_type,data[0].positions[4].position_objs,data[0].positions[5].position_objs))) {
 				callback({"Msg": "Invalid NAT positions."},null);
 				return;
 			}
 		}
 		else { // Filter policy
 			if (data.length != 1 || !(data[0].positions)
-					|| !(data[0].positions[0].ipobjs) || !(data[0].positions[1].ipobjs) || !(data[0].positions[2].ipobjs)
-					|| (policy_type === POLICY_TYPE_FORWARD && !(data[0].positions[3].ipobjs))) {
+					|| !(data[0].positions[0].position_objs) || !(data[0].positions[1].position_objs) || !(data[0].positions[2].position_objs)
+					|| (policy_type === POLICY_TYPE_FORWARD && !(data[0].positions[3].position_objs))) {
 				callback({"Msg": "Bad rule data."},null);
 				return;
 			}
