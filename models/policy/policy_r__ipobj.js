7f0bd23d2ad0d1152722b4829340d308e54a7b3f
obarranco <obarranco@soltecsis.com>
2018-04-05 15:36:47 +0200

    Creación de Clusters con Nodos de firewall y esctructura básica

--- models/policy/policy_r__ipobj.js
+++ models/policy/policy_r__ipobj.js
@@ -83,26 +83,26 @@ policy_r__ipobjModel.getPolicy_r__ipobjs_interfaces_positionPro = function (posi
                     ' inner join ipobj O on O.id=P.ipobj ' +
                     ' WHERE rule=' + connection.escape(position.rule) + ' AND position=' + connection.escape(position.id) +
                     ' AND O.type<>8 ' +
-                    ' UNION ' +  //SELECT IPOBJ UNDER HOST/INTERFACE
+                    ' UNION ' + //SELECT IPOBJ UNDER HOST/INTERFACE
                     ' SELECT ' + position.fwcloud + ' as fwcloud, ' + position.firewall + ' as firewall,  rule, OF.id as ipobj, P.ipobj_g, P.interface, position, position_order, negate, "O" as type ' +
                     ' FROM ' + tableModel + ' P ' +
                     ' inner join ipobj O on O.id=P.ipobj ' +
                     ' inner join interface__ipobj II on II.ipobj=O.id ' +
-                    ' inner join interface I on I.id=II.interface ' + 
+                    ' inner join interface I on I.id=II.interface ' +
                     ' inner join ipobj OF on OF.interface=I.id ' +
                     ' WHERE rule=' + connection.escape(position.rule) + ' AND position=' + connection.escape(position.id) +
-                    ' AND O.type=8 ' +                    
-                    ' UNION ' +  //SELECT IPOBJ UNDER GROUP
+                    ' AND O.type=8 ' +
+                    ' UNION ' + //SELECT IPOBJ UNDER GROUP
                     ' SELECT ' + position.fwcloud + ' as fwcloud, ' + position.firewall + ' as firewall,  rule, O.id as ipobj, P.ipobj_g, P.interface, position, position_order, negate, "O" as type ' +
                     ' FROM ' + tableModel + ' P ' +
                     ' inner join ipobj__ipobjg G on G.ipobj_g=P.ipobj_g ' +
-                    ' inner join ipobj O on O.id=G.ipobj ' +                    
-                    ' WHERE rule=' + connection.escape(position.rule) + ' AND position=' + connection.escape(position.id) +                    
-                    ' UNION ' +  //SELECT IPOBJ UNDER INTERFACE
-                    ' SELECT ' + position.fwcloud + ' as fwcloud, ' + position.firewall + ' as firewall, rule, O.id as ipobj,-1,-1,position,position_order, negate, "I" as type ' + 
+                    ' inner join ipobj O on O.id=G.ipobj ' +
+                    ' WHERE rule=' + connection.escape(position.rule) + ' AND position=' + connection.escape(position.id) +
+                    ' UNION ' + //SELECT IPOBJ UNDER INTERFACE
+                    ' SELECT ' + position.fwcloud + ' as fwcloud, ' + position.firewall + ' as firewall, rule, O.id as ipobj,-1,-1,position,position_order, negate, "I" as type ' +
                     ' FROM policy_r__interface P ' +
-                    ' inner join interface I on I.id=P.interface ' + 
-                    ' inner join ipobj O on O.interface=I.id ' + 
+                    ' inner join interface I on I.id=P.interface ' +
+                    ' inner join ipobj O on O.interface=I.id ' +
                     ' WHERE rule=' + connection.escape(position.rule) + ' AND position=' + connection.escape(position.id) +
                     ' ORDER BY position_order';
             //logger.debug("BUSCANDO OBJETOS EN POSITION: ", position.name, "  -> ", sql);
@@ -110,21 +110,24 @@ policy_r__ipobjModel.getPolicy_r__ipobjs_interfaces_positionPro = function (posi
                 if (error)
                     reject(error);
                 else {
-                    //logger.debug("DENTRO de BUCLE de IPOBJS dentro de POSITION: " + position.id + " FWCLOUD: " + position.fwcloud + " --> " + rows.length + " IPOBJS");
-                    Promise.all(rows.map(IpobjModel.getFinalIpobjPro))
-                            .then(dataI => {
-                                position.ipobjs = dataI;
-                                //logger.debug("-------------------------> FINAL de POSITIONS : " + position.id + " ----");
-                                resolve({"id": position.id, "name": position.name, "position_order": position.position_order, "position_objs": dataI});
-                            })
-                            .catch(e => {
-                                reject(e);
-                            });
+                    getNegateRulePosition(position.rule, position.id, function (error, negate) {                        
+                        //logger.debug("DENTRO de BUCLE de IPOBJS dentro de POSITION: " + position.id + " FWCLOUD: " + position.fwcloud + " --> " + rows.length + " IPOBJS");
+                        Promise.all(rows.map(IpobjModel.getFinalIpobjPro))
+                                .then(dataI => {
+                                    position.ipobjs = dataI;
+                                    //logger.debug("-------------------------> FINAL de POSITIONS : " + position.id + " ----");
+                                    resolve({"id": position.id, "name": position.name, "position_order": position.position_order, "negate" : negate, "position_objs": dataI});
+                                })
+                                .catch(e => {
+                                    reject(e);
+                                });
+                    });
                 }
             });
         });
     });
 };
+
 
 //Get All policy_r__ipobj by Policy_r (rule) and position
 policy_r__ipobjModel.getPolicy_r__ipobjs_position_data = function (rule, position, callback) {
