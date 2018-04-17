06c3286e40356077d3c87b6244f16630e678b942
obarranco <obarranco@soltecsis.com>
2018-04-17 16:14:29 +0200

    añadir protocolo segun tipo

--- routes/ipobj/ipobjs.js
+++ routes/ipobj/ipobjs.js
@@ -103,6 +103,7 @@ var objModel = 'IPOBJ';
  */
 var logger = require('log4js').getLogger("app");
 
+var Ipobj_typeModel = require('../../models/ipobj/ipobj_type');
 
 /**
  * Get all ipobjs by  group
@@ -559,45 +560,58 @@ router.post("/ipobj/:node_parent/:node_order/:node_type", function (req, res)
         ipobjData = obj;
     });
 
+    //GET PROTOCOL NUMBER FROM IPOBJ_TYPE
 
-    IpobjModel.insertIpobj(ipobjData, function (error, data)
-    {
+    Ipobj_typeModel.getIpobj_type(ipobjData.type, function (error, data) {
         if (error)
-            api_resp.getJson(data, api_resp.ACR_ERROR, '', objModel, error, function (jsonResp) {
+            api_resp.getJson(data, api_resp.ACR_DATA_ERROR, 'Error inserting IPOBJ', objModel, error, function (jsonResp) {
                 res.status(200).json(jsonResp);
             });
         else {
-            //If saved ipobj Get data
-            if (data && data.insertId > 0)
-            {
-                IpobjModel.UpdateHOST(data.insertId)
-                        .then(IpobjModel.UpdateINTERFACE(data.insertId))
-                        .then(() => {
-                            var id = data.insertId;
-                            logger.debug("NEW IPOBJ id:" + id + "  Type:" + ipobjData.type + "  Name:" + ipobjData.name);
-                            ipobjData.id = id;
-                            //INSERT IN TREE
-                            fwcTreemodel.insertFwc_TreeOBJ(iduser, fwcloud, node_parent, node_order, node_type, ipobjData, function (error, data) {
-                                if (data && data.insertId) {
-                                    //res.status(200).json({"insertId": id, "TreeinsertId": data.insertId});
-                                    var dataresp = {"insertId": id, "TreeinsertId": data.insertId};
-                                    api_resp.getJson(dataresp, api_resp.ACR_INSERTED_OK, 'IPOBJ INSERTED OK', objModel, null, function (jsonResp) {
-                                        res.status(200).json(jsonResp);
-                                    });
-
-                                } else {
-                                    api_resp.getJson(data, api_resp.ACR_ERROR, 'Error inserting TREE NODE IPOBJ', objModel, error, function (jsonResp) {
-                                        res.status(200).json(jsonResp);
-                                    });
-                                }
-                            })
-                        });
-            } else
-            {
-                api_resp.getJson(data, api_resp.ACR_DATA_ERROR, 'Error inserting IPOBJ', objModel, error, function (jsonResp) {
-                    res.status(200).json(jsonResp);
-                });
+            if (data && data[0].protocol_number!==null){
+                ipobjData.protocol= data[0].protocol_number;
             }
+            logger.debug(ipobjData);
+            IpobjModel.insertIpobj(ipobjData, function (error, data)
+            {
+                if (error)
+                    api_resp.getJson(data, api_resp.ACR_ERROR, '', objModel, error, function (jsonResp) {
+                        res.status(200).json(jsonResp);
+                    });
+                else {
+                    //If saved ipobj Get data
+                    if (data && data.insertId > 0)
+                    {
+                        IpobjModel.UpdateHOST(data.insertId)
+                                .then(IpobjModel.UpdateINTERFACE(data.insertId))
+                                .then(() => {
+                                    var id = data.insertId;
+                                    logger.debug("NEW IPOBJ id:" + id + "  Type:" + ipobjData.type + "  Name:" + ipobjData.name);
+                                    ipobjData.id = id;
+                                    //INSERT IN TREE
+                                    fwcTreemodel.insertFwc_TreeOBJ(iduser, fwcloud, node_parent, node_order, node_type, ipobjData, function (error, data) {
+                                        if (data && data.insertId) {
+                                            //res.status(200).json({"insertId": id, "TreeinsertId": data.insertId});
+                                            var dataresp = {"insertId": id, "TreeinsertId": data.insertId};
+                                            api_resp.getJson(dataresp, api_resp.ACR_INSERTED_OK, 'IPOBJ INSERTED OK', objModel, null, function (jsonResp) {
+                                                res.status(200).json(jsonResp);
+                                            });
+
+                                        } else {
+                                            api_resp.getJson(data, api_resp.ACR_ERROR, 'Error inserting TREE NODE IPOBJ', objModel, error, function (jsonResp) {
+                                                res.status(200).json(jsonResp);
+                                            });
+                                        }
+                                    });
+                                });
+                    } else
+                    {
+                        api_resp.getJson(data, api_resp.ACR_DATA_ERROR, 'Error inserting IPOBJ', objModel, error, function (jsonResp) {
+                            res.status(200).json(jsonResp);
+                        });
+                    }
+                }
+            });
         }
     });
 
