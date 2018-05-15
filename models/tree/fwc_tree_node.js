6239dea22a9721163a70cf964da16c1c757b9ad2
Carles Xavier Munyoz Baldó <cmunyoz@soltecsis.com>
2018-05-15 12:50:39 +0200

    Add status_installed in node tree info.

--- models/tree/fwc_tree_node.js
+++ models/tree/fwc_tree_node.js
@@ -2,31 +2,32 @@
 
 
 function fwc_tree_node(node_data) {
-    try {
-        this.id = node_data.id;
-        this.text = node_data.name;
-        this.pid = node_data.id_parent;
-        this.node_order = node_data.node_order;
-        this.icon = node_data.node_icon;
-        this.allowdrag = 0;
-        this.expanded = node_data.expanded;
-        this.node_type = node_data.node_type;
-        this.api_call = node_data.api_call;
-        this.obj_type = node_data.obj_type;
-        this.id_obj = node_data.id_obj;
-        this.node_level = node_data.node_level;
-        this.fwcloud = node_data.fwcloud;
-        this.show_action = node_data.show_action;
-        this.status_compiled = node_data.status_compiled;
-        try {
-            this.fwcloud_tree = node_data.fwcloud_tree;
-        } catch (err) {
-            this.fwcloud_tree = node_data.fwcloud;
-        }
-    } catch (err) {
-        // Handle the error here.
+	try {
+		this.id = node_data.id;
+		this.text = node_data.name;
+		this.pid = node_data.id_parent;
+		this.node_order = node_data.node_order;
+		this.icon = node_data.node_icon;
+		this.allowdrag = 0;
+		this.expanded = node_data.expanded;
+		this.node_type = node_data.node_type;
+		this.api_call = node_data.api_call;
+		this.obj_type = node_data.obj_type;
+		this.id_obj = node_data.id_obj;
+		this.node_level = node_data.node_level;
+		this.fwcloud = node_data.fwcloud;
+		this.show_action = node_data.show_action;
+		this.status_compiled = node_data.status_compiled;
+		this.status_installed = node_data.status_installed;
+		try {
+			this.fwcloud_tree = node_data.fwcloud_tree;
+		} catch (err) {
+			this.fwcloud_tree = node_data.fwcloud;
+		}
+	} catch (err) {
+		// Handle the error here.
 
-    }
+	}
 }
 
 
