8f97661e81cd1ea18493b74491788fb406e07942
Carles Xavier Munyoz Baldó <cmunyoz@soltecsis.com>
2018-10-30 15:59:33 +0100

    #246

--- middleware/confirmation_token.js
+++ middleware/confirmation_token.js
@@ -8,7 +8,7 @@ var utilsModel = require('../utils/utils');
 var userModel = require('../models/user/user');
 
 confirmToken.check = async (req, res, next) => {
-  if (req.url.split('/').pop()==='get' || req.method==='GET' || (req.method==='POST' && req.path==='/users/login'))
+  if (req.url.split('/').pop()==='get' || req.method==='GET' || (req.method==='POST' && req.path==='/user/login'))
     return next();
 
   try {
