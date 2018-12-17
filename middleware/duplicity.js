2b25b87caca2745f476e685fc164c7829a1074b8
Carles Xavier Munyoz Baldó <cmunyoz@soltecsis.com>
2018-12-17 13:20:01 +0100

    #332

--- middleware/duplicity.js
+++ middleware/duplicity.js
@@ -19,23 +19,23 @@ duplicityCheck.ipobj = (req, res, next) => {
 
 	var sql = 'SELECT id,name FROM ipobj' +
 		' WHERE (fwcloud IS NULL OR fwcloud=' + req.body.fwcloud + ")" + 
-		' AND type' + (!req.body.type ? " IS NULL" : ("="+req.body.type)) +
-		' AND protocol' + (!req.body.protocol ? " IS NULL" : ("="+req.body.protocol)) +
-		' AND address' + (!req.body.address ? " IS NULL" : ("="+req.dbCon.escape(req.body.address))) +
-		' AND netmask' + (!req.body.netmask ? " IS NULL" : ("="+req.dbCon.escape(req.body.netmask))) +
-		' AND diff_serv' + (!req.body.diff_serv ? " IS NULL" : ("="+req.body.diff_serv)) +
-		' AND ip_version' + (!req.body.ip_version ? " IS NULL" : ("="+req.body.ip_version)) +
-		' AND icmp_type' + (!req.body.icmp_type ? " IS NULL" : ("="+req.body.icmp_type)) +
-		' AND icmp_code' + (!req.body.icmp_code ? " IS NULL" : ("="+req.body.icmp_code)) +
-		' AND tcp_flags_mask' + (!req.body.tcp_flags_mask ? " IS NULL" : ("="+req.body.tcp_flags_mask)) +
-		' AND tcp_flags_settings' + (!req.body.tcp_flags_settings ? " IS NULL" : ("="+req.body.tcp_flags_settings)) +
-		' AND range_start' + (!req.body.range_start ? " IS NULL" : ("="+req.dbCon.escape(req.body.range_start))) +
-		' AND range_end' + (!req.body.range_end ? " IS NULL" : ("="+req.dbCon.escape(req.body.range_end))) +
-		' AND source_port_start' + (!req.body.source_port_start ? " IS NULL" : ("="+req.body.source_port_start)) +
-		' AND source_port_end' + (!req.body.source_port_end ? " IS NULL" : ("="+req.body.source_port_end)) +
-		' AND destination_port_start' + (!req.body.destination_port_start ? " IS NULL" : ("="+req.body.destination_port_start)) +
-		' AND destination_port_end' + (!req.body.destination_port_end ? " IS NULL" : ("="+req.body.destination_port_end)) +
-		' AND options' + (!req.body.options ? " IS NULL" : ("="+req.body.options)) +
+		' AND type' + ((typeof req.body.type==='undefined' || req.body.type===null) ? " IS NULL" : ("="+req.body.type)) +
+		' AND protocol' + ((typeof req.body.protocol==='undefined' || req.body.protocol===null) ? " IS NULL" : ("="+req.body.protocol)) +
+		' AND address' + ((typeof req.body.address==='undefined' || req.body.address===null) ? " IS NULL" : ("="+req.dbCon.escape(req.body.address))) +
+		' AND netmask' + ((typeof req.body.netmask==='undefined' || req.body.netmask===null) ? " IS NULL" : ("="+req.dbCon.escape(req.body.netmask))) +
+		' AND diff_serv' + ((typeof req.body.diff_serv==='undefined' || req.body.diff_serv===null) ? " IS NULL" : ("="+req.body.diff_serv)) +
+		' AND ip_version' + ((typeof req.body.ip_version==='undefined' || req.body.ip_version===null) ? " IS NULL" : ("="+req.body.ip_version)) +
+		' AND icmp_type' + ((typeof req.body.icmp_type==='undefined' || req.body.icmp_type===null) ? " IS NULL" : ("="+req.body.icmp_type)) +
+		' AND icmp_code' + ((typeof req.body.icmp_code==='undefined' || req.body.icmp_code===null) ? " IS NULL" : ("="+req.body.icmp_code)) +
+		' AND tcp_flags_mask' + ((typeof req.body.tcp_flags_mask==='undefined' || req.body.tcp_flags_mask===null) ? " IS NULL" : ("="+req.body.tcp_flags_mask)) +
+		' AND tcp_flags_settings' + ((typeof req.body.tcp_flags_settings==='undefined' || req.body.tcp_flags_settings===null) ? " IS NULL" : ("="+req.body.tcp_flags_settings)) +
+		' AND range_start' + ((typeof req.body.range_start==='undefined' || req.body.range_start===null) ? " IS NULL" : ("="+req.dbCon.escape(req.body.range_start))) +
+		' AND range_end' + ((typeof req.body.range_end==='undefined' || req.body.range_end===null) ? " IS NULL" : ("="+req.dbCon.escape(req.body.range_end))) +
+		' AND source_port_start' + ((typeof req.body.source_port_start==='undefined' || req.body.source_port_start===null) ? " IS NULL" : ("="+req.body.source_port_start)) +
+		' AND source_port_end' + ((typeof req.body.source_port_end==='undefined' || req.body.source_port_end===null) ? " IS NULL" : ("="+req.body.source_port_end)) +
+		' AND destination_port_start' + ((typeof req.body.destination_port_start==='undefined' || req.body.destination_port_start===null) ? " IS NULL" : ("="+req.body.destination_port_start)) +
+		' AND destination_port_end' + ((typeof req.body.destination_port_end==='undefined' || req.body.destination_port_end===null) ? " IS NULL" : ("="+req.body.destination_port_end)) +
+		' AND options' + ((typeof req.body.options==='undefined' || req.body.options===null) ? " IS NULL" : ("="+req.body.options)) +
     (req.body.id ? ' AND id!='+req.body.id : '') +
 		' AND interface IS NULL';
 
