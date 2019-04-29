dddea9a7246f2dced85793938da2c662111dee8f
Carles Xavier Munyoz Baldó <cmunyoz@soltecsis.com>
2019-04-29 19:04:04 +0200

    Remove negate field in polilcy_r__ipobj.

--- config/data_ini/fwcloud.sql
+++ config/data_ini/fwcloud.sql
@@ -1442,7 +1442,6 @@ CREATE TABLE `policy_r__ipobj` (
   `interface` int(11) DEFAULT '0' COMMENT 'ID Interface in this position',
   `position` int(11) NOT NULL,
   `position_order` int(11) NOT NULL,
-  `negate` tinyint(1) NOT NULL DEFAULT '0',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `created_by` int(11) NOT NULL DEFAULT '0',
