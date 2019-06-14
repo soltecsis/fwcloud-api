ed984fe687fbda4e31cf51441a1c67cee9d71f4b
Felipe Pérez <fperez@soltecsis.com>
2019-06-14 12:51:39 +0200

    Fix missing slash in route policy--positions--get

--- routes/policy/positions.js
+++ routes/policy/positions.js
@@ -4,15 +4,15 @@ var policyPositionModel = require('../../models/policy/position');
 const fwcError = require('../../utils/error_table');
 
 /* Get all policy_positions by Type*/
-router.put('get', async (req, res) => {
-	try {
-		const data = await policyPositionModel.getPolicyPositionsByType(req.dbCon,req.body.type);
-		//If exists policy_position get data
-    if (data && data.length > 0)
-      res.status(200).json(data);
-    else
-			res.status(400).json(fwcError.NOT_FOUND);
-	} catch(error) { res.status(400).json(error) }
+router.put('/get', async(req, res) => {
+    try {
+        const data = await policyPositionModel.getPolicyPositionsByType(req.dbCon, req.body.type);
+        //If exists policy_position get data
+        if (data && data.length > 0)
+            res.status(200).json(data);
+        else
+            res.status(400).json(fwcError.NOT_FOUND);
+    } catch (error) { res.status(400).json(error) }
 });
 
 module.exports = router;
