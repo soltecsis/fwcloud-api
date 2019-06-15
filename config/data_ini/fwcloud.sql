8985c3506ed851571d42024fe6a9639a7da43940
Carles Munyoz <carles@MacBook-Pro-de-Carles.local>
2019-06-16 01:12:14 +0200

    Remove trigger.

--- config/data_ini/fwcloud.sql
+++ config/data_ini/fwcloud.sql
@@ -549,26 +549,6 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `fwcloud`.`ipobj_AFTER_UPDATE` AFTER UPDATE ON `ipobj` FOR EACH ROW
-BEGIN
-	UPDATE policy_r__ipobj set updated_at=CURRENT_TIMESTAMP WHERE ipobj=NEW.id ;
-    UPDATE ipobj__ipobjg  set updated_at=CURRENT_TIMESTAMP WHERE ipobj=NEW.id ;
-	UPDATE fwcloud set updated_at=CURRENT_TIMESTAMP WHERE id=NEW.fwcloud;
-END */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
 /*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `fwcloud`.`ipobj_AFTER_DELETE` AFTER DELETE ON `ipobj` FOR EACH ROW
 BEGIN
 	UPDATE fwcloud set updated_at=CURRENT_TIMESTAMP WHERE id=OLD.fwcloud;
