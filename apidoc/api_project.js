a3c3cbf18417e97ff610eec94b625198e6e4f3a3
Carles Xavier Munyoz Baldó <cmunyoz@soltecsis.com>
2019-06-18 11:05:50 +0200

    Add license header.

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,1 +1,24 @@
+/*
+    Copyright 2019 SOLTECSIS SOLUCIONES TECNOLOGICAS, SLU
+    https://soltecsis.com
+    info@soltecsis.com
+
+
+    This file is part of FWCloud (https://fwcloud.net).
+
+    FWCloud is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FWCloud is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FWCloud.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 define({  "title": "FWCloud.net API",  "url": "https://api.fwcloud.net",  "template": {    "forceLanguage": "en",    "withCompare": false,    "withGenerator": false  },  "name": "FWCloud.net-API",  "version": "0.8.5",  "description": "Documentation for the RESTful API of the FWCloud.net project.",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-06-09T17:54:42.563Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
