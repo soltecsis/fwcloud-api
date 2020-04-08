71158dd03fdf07b8e85aaa16f299927911902781
Jordi <jurios@soltecsis.com>
2020-04-08 12:18:26 +0200

    Fix exporters

--- src/snapshots/exporter/ipobj.exporter.ts
+++ src/snapshots/exporter/ipobj.exporter.ts
@@ -21,12 +21,13 @@
 */
 
 import { TableExporter } from "./table-exporter";
-import { Connection, SelectQueryBuilder, QueryBuilder } from "typeorm";
+import { Connection, SelectQueryBuilder, QueryBuilder, Brackets } from "typeorm";
 import { IPObj } from "../../models/ipobj/IPObj";
 import { FwCloud } from "../../models/fwcloud/FwCloud";
 import Model from "../../models/Model";
 import { InterfaceExporter } from "./interface.exporter";
 import { Interface } from "../../models/interface/Interface";
+import { FwCloudExporter } from "./fwcloud-exporter";
 
 export class IPObjExporter extends TableExporter {
     protected getEntity(): typeof Model {
@@ -35,14 +36,23 @@ export class IPObjExporter extends TableExporter {
 
     public getFilterBuilder(qb: SelectQueryBuilder<any>, alias: string, fwCloudId: number): SelectQueryBuilder<any> {
         return qb
-        .where(`${alias}.id >= 100000`)
-        .leftJoin(FwCloud, "fwcloud", "fwcloud.id = :id", {id: fwCloudId})
-        .leftJoin("ipobj.interface", "interface")
-        .where((qb) => {
-            const subquery = qb.subQuery().from(Interface, 'interface').select('interface.id');
+            .where(`${alias}.id >= 100000`)
+            .andWhere(new Brackets(qb => {
+                qb.where(qb => {
+                    qb
+                        .where(qb => {
+                            const subquery = qb.subQuery().from(FwCloud, 'fwcloud').select('fwcloud.id');
 
-            return 'interface.id IN ' + new InterfaceExporter()
-                .getFilterBuilder(subquery, 'interface', fwCloudId).getQuery()
-        });
+                            return `${alias}.fwCloudId IN ` + new FwCloudExporter()
+                                .getFilterBuilder(subquery, 'fwcloud', fwCloudId).getQuery()
+                        })
+                        .orWhere(qb => {
+                            const subquery = qb.subQuery().from(Interface, 'interface').select('interface.id');
+
+                            return `${alias}.interfaceId IN ` + new InterfaceExporter()
+                                .getFilterBuilder(subquery, 'interface', fwCloudId).getQuery()
+                        })
+                });
+            }));
     }
 }
