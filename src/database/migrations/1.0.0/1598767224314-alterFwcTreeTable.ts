3838ba94c019aa5dcfdf037104890919135da054
Felipe Pérez <fperez@soltecsis.com>
2020-08-30 08:16:58 +0200

    Add migration to alter table fwc_tree

---
+++ src/database/migrations/1.0.0/1598767224314-alterFwcTreeTable.ts
@@ -0,0 +1,13 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class alterFwcTreeTable1598767224314 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`ALTER TABLE fwc_tree MODIFY COLUMN name VARCHAR(255)`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`ALTER TABLE fwc_tree MODIFY COLUMN name VARCHAR(45)`);
+    }
+
+}
