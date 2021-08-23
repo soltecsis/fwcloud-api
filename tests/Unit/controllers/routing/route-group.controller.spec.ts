15d0b230624f214abc9e2ccb075f18cf4942c3a8
Jordi <jurios@soltecsis.com>
2021-08-23 12:59:26 +0200

    throw error if route group does not exist

--- tests/Unit/controllers/routing/route-group.controller.spec.ts
+++ tests/Unit/controllers/routing/route-group.controller.spec.ts
@@ -1,18 +1,13 @@
-import { getRepository } from "typeorm";
+import { getRepository, QueryFailedError } from "typeorm";
 import { Application } from "../../../../src/Application";
 import { RoutingRuleController } from "../../../../src/controllers/routing/routing-rule/routing-rule.controller";
 import { Firewall } from "../../../../src/models/firewall/Firewall";
-import { FirewallService } from "../../../../src/models/firewall/firewall.service";
-import { RoutingRule } from "../../../../src/models/routing/routing-rule/routing-rule.model";
-import { RoutingRuleService } from "../../../../src/models/routing/routing-rule/routing-rule.service";
-import { RoutingTable } from "../../../../src/models/routing/routing-table/routing-table.model";
 import { RoutingTableService } from "../../../../src/models/routing/routing-table/routing-table.service";
 import StringHelper from "../../../../src/utils/string.helper";
 import { expect, testSuite } from "../../../mocha/global-setup";
 import { FwCloudFactory, FwCloudProduct } from "../../../utils/fwcloud-factory";
 import { Request } from 'express';
 import Sinon from "sinon";
-import { RoutingRulePolicy } from "../../../../src/policies/routing-rule.policy";
 import { Tree } from "../../../../src/models/tree/Tree";
 import { Authorization } from "../../../../src/fonaments/authorization/policy";
 import { RouteGroupController } from "../../../../src/controllers/routing/route-group/route-group.controller";
@@ -21,6 +16,7 @@ import { RouteGroup } from "../../../../src/models/routing/route-group/route-gro
 import { RouteGroupService } from "../../../../src/models/routing/route-group/route-group.service";
 import { RouteGroupPolicy } from "../../../../src/policies/route-group.policy";
 import { RequestInputs } from "../../../../src/fonaments/http/request-inputs";
+import { FwCloud } from "../../../../src/models/fwcloud/FwCloud";
 
 describe(RoutingRuleController.name, () => {
     let controller: RouteGroupController;
@@ -57,6 +53,95 @@ describe(RoutingRuleController.name, () => {
         } as unknown as Request);
     });
 
+    describe('make', () => {
+        let group: RouteGroup;
+
+        beforeEach(async () => {
+            group = await getRepository(RouteGroup).save({
+                firewallId: firewall.id,
+                name: 'group'
+            })
+        })
+        it('should not throw error if the route belongs to the firewall which belongs to the fwcloud', async () => {
+            expect(await controller.make({
+                params: {
+                    fwcloud: fwcProduct.fwcloud.id,
+                    firewall: firewall.id,
+                    routeGroup: group.id
+                }
+            } as unknown as Request)).to.be.undefined;
+        });
+
+        it('should throw error if the route blongs to other firewall', async () => {
+            const newFirewall: Firewall = await getRepository(Firewall).save(getRepository(Firewall).create({
+                name: StringHelper.randomize(10),
+                fwCloudId: fwcProduct.fwcloud.id
+            }));
+
+            group = await getRepository(RouteGroup).create({
+                firewallId: newFirewall.id,
+                name: 'group'
+            })
+
+            expect(controller.make({
+                params: {
+                    fwcloud: fwcProduct.fwcloud.id,
+                    firewall: firewall.id,
+                    routeGroup: group.id
+                }
+            } as unknown as Request)).rejectedWith(QueryFailedError);
+        });
+
+        it('should throw error if the firewall blongs to other fwcloud', async () => {
+            const newFwcloud: FwCloud = await getRepository(FwCloud).save(getRepository(FwCloud).create({
+                name: StringHelper.randomize(10)
+            }));
+
+            const newFirewall: Firewall = await getRepository(Firewall).save(getRepository(Firewall).create({
+                name: StringHelper.randomize(10),
+                fwCloudId: newFwcloud.id
+            }));
+
+            
+            expect(controller.make({
+                params: {
+                    fwcloud: fwcProduct.fwcloud.id,
+                    firewall: newFirewall.id
+                }
+            } as unknown as Request)).rejectedWith(QueryFailedError);
+        });
+
+        it('should throw error if the fwcloud does not exist', async () => {
+            expect(controller.make({
+                params: {
+                    fwcloud: -1,
+                    firewall: firewall.id,
+                    routeGroup: group.id
+                }
+            } as unknown as Request)).rejectedWith(QueryFailedError);
+        });
+
+        it('should throw error if the firewall does not exist', async () => {
+            expect(controller.make({
+                params: {
+                    fwcloud: fwcProduct.fwcloud.id,
+                    firewall: -1,
+                    routeGroup: group.id
+                }
+            } as unknown as Request)).rejectedWith(QueryFailedError);
+        });
+
+        it('should throw error if the table does not exist', async () => {
+            expect(controller.make({
+                params: {
+                    fwcloud: fwcProduct.fwcloud.id,
+                    firewall: firewall.id,
+                    routeGroup: -1
+                }
+            } as unknown as Request)).rejectedWith(QueryFailedError);
+        });
+    })
+
     describe('update', () => {
         let group: RouteGroup;
 
@@ -68,6 +153,8 @@ describe(RoutingRuleController.name, () => {
             });
 
             const spy: Sinon.SinonSpy = Sinon.stub(RouteGroupPolicy, 'update').resolves(Authorization.grant());
+
+            controller["_routeGroup"] = group;
         });
 
         it('should handle updates without changing routes', async () => {
