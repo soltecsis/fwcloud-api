84f1f52e187c67184459aa8aa547728bc9a33f8b
Jordi <jurios@soltecsis.com>
2021-10-18 15:22:01 +0200

    fix

--- src/models/vpn/openvpn/status/worker.ts
+++ src/models/vpn/openvpn/status/worker.ts
@@ -3,27 +3,37 @@ import { Application } from "../../../../Application";
 import { AgentCommunication } from "../../../../communications/agent.communication";
 import { OpenVPNHistoryRecord } from "../../../../communications/communication";
 import db from "../../../../database/database-manager";
-import { FirewallInstallCommunication } from "../../../firewall/Firewall";
+import { Firewall, FirewallInstallCommunication } from "../../../firewall/Firewall";
 import { OpenVPN } from "../OpenVPN";
 import { OpenVPNOption } from "../openvpn-option.model";
 import { OpenVPNStatusHistoryService } from "./openvpn-status-history.service";
 
 async function iterate(application: Application): Promise<void> {
-    const service: OpenVPNStatusHistoryService = await application.getService(OpenVPNStatusHistoryService.name);
+    try {
+        const service: OpenVPNStatusHistoryService = await application.getService(OpenVPNStatusHistoryService.name);
 
-    const openvpns: OpenVPN[] = await getRepository(OpenVPN).createQueryBuilder('openvpn')
-        .innerJoin('openvpn.crt', 'crt')
-        .innerJoinAndSelect('openvpn.firewall', 'firewall')
-        .where('openvpn.parentId IS NULL')
-        .andWhere('crt.type =  2')
-        .andWhere('firewall.install_communication = :communication', {
-            communication: FirewallInstallCommunication.Agent
-        }).getMany();
+        const openvpns: OpenVPN[] = await getRepository(OpenVPN).createQueryBuilder('openvpn')
+            .innerJoin('openvpn.crt', 'crt')
+            .innerJoinAndSelect('openvpn.firewall', 'firewall')
+            .where('openvpn.parentId IS NULL')
+            .andWhere('crt.type =  2')
+            .andWhere('firewall.install_communication = :communication', {
+                communication: FirewallInstallCommunication.Agent
+            }).getMany();
 
         for(let openvpn of openvpns) {
-            try {
 
-                const communication: AgentCommunication = await openvpn.firewall.getCommunication() as AgentCommunication;
+            const firewalls: Firewall[] = openvpn.firewall.clusterId
+                ? await getRepository(Firewall).createQueryBuilder('firewall')
+                    .where('firewall.clusterId = :cluster', {cluster: openvpn.firewall.clusterId})
+                    .andWhere('firewall.install_communication = :communication', {
+                        communication: FirewallInstallCommunication.Agent
+                    })
+                    .getMany()
+                : [openvpn.firewall];
+
+            for(let firewall of firewalls) {
+                const communication: AgentCommunication = await firewall.getCommunication() as AgentCommunication;
 
                 const statusOption: OpenVPNOption = await OpenVPN.getOptData(db.getQuery(), openvpn.id, 'status') as OpenVPNOption;
 
@@ -40,10 +50,11 @@ async function iterate(application: Application): Promise<void> {
                         connectedAt: item.connectedAt
                     })));
                 }
-            } catch(e) {
-                application.logger().error(`WorkerError: ${e.message}`);
             }
         }
+    } catch(e) {
+        application.logger().error(`WorkerError: ${e.message}`);
+    }
 }
 
 async function work(): Promise<void> {
