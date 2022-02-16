0a3b470d14db18b2bf423ef44235c383d513f857
Jordi <jurios@soltecsis.com>
2022-02-16 12:01:50 +0100

    tests

--- tests/Unit/cli/commands/keys-generate.command.spec.ts
+++ tests/Unit/cli/commands/keys-generate.command.spec.ts
@@ -148,7 +148,7 @@ describe(describeName('KeysGenerateCommand tests'), () => {
         stubVar.restore()
     });
 
-    it('should overwrite CRYPT_SECRET if is defined', async () => {
+    it('should not overwrite CRYPT_SECRET if is defined', async () => {
         const stubVar = sinon.stub(KeysGenerateCommand, <any> 'ENV_FILENAME').value(testEnvPath);
         let envData: string = fse.readFileSync(testEnvPath).toString()
         envData = envData.replace(new RegExp('^CRYPT_SECRET=(.)*\n', 'm'), `CRYPT_SECRET=test\n`);
@@ -157,6 +157,25 @@ describe(describeName('KeysGenerateCommand tests'), () => {
         await runCLICommandIsolated(testSuite, async () => {
             return new KeysGenerateCommand().safeHandle({
             $0: "keys:generate",
+            _: []
+        })});
+
+        const envContent: string = readTestEnvContent();
+
+        expect(envContent).matches(new RegExp('CRYPT_SECRET=test'));
+        stubVar.restore()
+    });
+
+    it('should overwrite CRYPT_SECRET if force option is defined', async () => {
+        const stubVar = sinon.stub(KeysGenerateCommand, <any> 'ENV_FILENAME').value(testEnvPath);
+        let envData: string = fse.readFileSync(testEnvPath).toString()
+        envData = envData.replace(new RegExp('^CRYPT_SECRET=(.)*\n', 'm'), `CRYPT_SECRET=test\n`);
+        fse.writeFileSync(testEnvPath, envData);
+
+        await runCLICommandIsolated(testSuite, async () => {
+            return new KeysGenerateCommand().safeHandle({
+            $0: "keys:generate",
+            force: true,
             _: []
         })});
 
