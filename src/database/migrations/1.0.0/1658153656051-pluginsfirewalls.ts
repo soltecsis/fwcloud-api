43c260b0db2accb6588a92c75690aeabfb748eac
Ignacio Ortega Carrillo <iortega@soltecsis.com>
2022-07-26 16:12:27 +0200

    migration plugins cluster added

--- src/database/migrations/1.0.0/1658153656051-pluginsfirewalls.ts
+++ src/database/migrations/1.0.0/1658153656051-pluginsfirewalls.ts
@@ -12,10 +12,21 @@ export class pluginsfirewalls1658153656051 implements MigrationInterface {
                 default: 0
             }), 
         )
+
+        await queryRunner.addColumn('cluster',
+            new TableColumn({
+                name: 'plugins',
+                type: 'smallint',
+                length: "2",
+                isNullable: false,
+                default: 0
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropColumn('firewall','plugin')
+        await queryRunner.dropColumn('firewall','plugins')
+        await queryRunner.dropColumn('cluster','plugins')
     }
 
 }
