dd2acb8a0a2f50993dafe386c40cbab01e463ac8
Carles Munyoz <cmunyoz@soltecsis.com>
2022-12-01 11:51:58 +0100

    Tests bug fix

--- src/database/migrations/1.0.0/1669884249679-add_openvpn_status_history_indexes.ts
+++ src/database/migrations/1.0.0/1669884249679-add_openvpn_status_history_indexes.ts
@@ -9,7 +9,7 @@ export class addOpenvpnStatusHistoryIndexes1669884249679 implements MigrationInt
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query("ALTER TABLE openvpn_status_history DROP INDEX IDX_d5f7fe1875fc92dba78c500371");
-        await queryRunner.query("ALTER TABLE openvpn_status_history DROP INDEX IDX_adba662cf32738c010d418fda8");
+        await queryRunner.query("set FOREIGN_KEY_CHECKS=0; ALTER TABLE openvpn_status_history DROP INDEX IDX_adba662cf32738c010d418fda8; set FOREIGN_KEY_CHECKS=1;");
     }
 
 }
