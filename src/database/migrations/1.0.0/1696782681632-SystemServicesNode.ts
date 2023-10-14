07bb45768017d07d8b31cba04856f7acd3693047
iortega <iortega@soltecsis.com>
2023-10-14 20:30:52 +0200

    Migration further developed

--- src/database/migrations/1.0.0/1696782681632-SystemServicesNode.ts
+++ src/database/migrations/1.0.0/1696782681632-SystemServicesNode.ts
@@ -1,25 +1,72 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class SystemServicesNode1696782681632 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        const tables = [
-            { tableName: 'firewall', objType: 1 },
-            { tableName: 'cluster', objType: 2 }
-        ];
+        await queryRunner.query(
+            "INSERT INTO `fwc_tree_node_types` (`node_type`, `obj_type`, `name`) VALUES( 'SYS', NULL, 'System')"
+        );
 
-        for (let table of tables) {
-            const items = await queryRunner.query(`SELECT id FROM ${table.tableName}`);
+        await queryRunner.createTable(new Table({
+            name: 'system',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'int',
+                    isGenerated: true,
+                    generationStrategy: 'increment',
+                    isPrimary: true
+                },
+                {
+                    name: 'firewall_cluster_id',
+                    type: 'int',
+                    isNullable: false
+                },
+                {
+                    name: 'node_type',
+                    type: 'varchar',
+                    isNullable: false
+                }
+            ],
+            indices: [{ columnNames: ['firewall'], isUnique: true }],
+            foreignKeys: [
+                {
+                    columnNames: ['firewall'],
+                    referencedTableName: 'firewall',
+                    referencedColumnNames: ['id']
+                }
+            ]
+        }));
 
-            for (let item of items) {
-                await queryRunner.query(`INSERT INTO fwc_tree (name, id_parent, node_type, id_obj, obj_type) VALUES (?, NULL, 'SYS', ?, ?)`, ['System', item.id, table.objType]);
-            }
+        // Retrieve all existing firewalls and clusters
+        const firewallsAndClusters = await queryRunner.query("SELECT id FROM firewall UNION SELECT id FROM cluster");
+
+        for (const item of firewallsAndClusters) {
+            // Insert an entry in the system table for each firewall and cluster
+            await queryRunner.query(
+                "INSERT INTO system (firewall_cluster_id, node_type) VALUES (?, 'System')",
+                [item.id]
+            );
+
+            // Retrieve the id of the newly created system entry
+            const [systemEntry] = await queryRunner.query(
+                "SELECT id FROM system WHERE firewall_cluster_id = ? AND node_type = 'System'",
+                [item.id]
+            );
+
+            // Create child nodes: DHCP, Keepalived, and HAProxy for the system entry."
+            await queryRunner.query(
+                "INSERT INTO system (firewall_cluster_id, node_type) VALUES (?, 'DHCP'), (?, 'Keepalived'), (?, 'HAProxy')",
+                [systemEntry.id, systemEntry.id, systemEntry.id]
+            );
         }
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        for (let table of tables) {
-            await queryRunner.query(`DELETE FROM fwc_tree WHERE node_type='SYS' AND obj_type=?`, [table.objType]);
-        }
+        await queryRunner.query(
+            "DELETE FROM `fwc_tree_node_types` WHERE `node_type` = 'SYS' AND `name` = 'System'"
+        );
+
+        await queryRunner.dropTable('system');
     }
 }
