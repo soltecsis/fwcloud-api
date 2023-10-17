7245faf7ff8a4c129f85915efe0568544204dac9
iortega <iortega@soltecsis.com>
2023-10-17 21:04:12 +0200

    Solved migration

--- src/database/migrations/1.0.0/1696782681632-SystemServicesNode.ts
+++ src/database/migrations/1.0.0/1696782681632-SystemServicesNode.ts
@@ -7,66 +7,79 @@ export class SystemServicesNode1696782681632 implements MigrationInterface {
             "INSERT INTO `fwc_tree_node_types` (`node_type`, `obj_type`, `name`) VALUES( 'SYS', NULL, 'System')"
         );
 
-        await queryRunner.createTable(new Table({
-            name: 'system',
-            columns: [
-                {
-                    name: 'id',
-                    type: 'int',
-                    isGenerated: true,
-                    generationStrategy: 'increment',
-                    isPrimary: true
-                },
-                {
-                    name: 'firewall_cluster_id',
-                    type: 'int',
-                    isNullable: false
-                },
-                {
-                    name: 'node_type',
-                    type: 'varchar',
-                    isNullable: false
-                }
-            ],
-            indices: [{ columnNames: ['firewall'], isUnique: true }],
-            foreignKeys: [
-                {
-                    columnNames: ['firewall'],
-                    referencedTableName: 'firewall',
-                    referencedColumnNames: ['id']
-                }
-            ]
-        }));
+        await queryRunner.query(
+            "INSERT INTO `fwc_tree_node_types` (`node_type`, `obj_type`, `name`) VALUES( 'SYS', NULL, 'DHCP')"
+        );
 
-        // Retrieve all existing firewalls and clusters
-        const firewallsAndClusters = await queryRunner.query("SELECT id FROM firewall UNION SELECT id FROM cluster");
+        await queryRunner.query(
+            "INSERT INTO `fwc_tree_node_types` (`node_type`, `obj_type`, `name`) VALUES( 'SYS', NULL, 'Keepalived')"
+        );
 
-        for (const item of firewallsAndClusters) {
-            // Insert an entry in the system table for each firewall and cluster
+        await queryRunner.query(
+            "INSERT INTO `fwc_tree_node_types` (`node_type`, `obj_type`, `name`) VALUES( 'SYS', NULL, 'HAProxy')"
+        );
+
+        const firewalls = await queryRunner.query(
+            "SELECT `id` FROM `fwc_tree` WHERE `node_type` = 'firewall'"
+        );
+
+        for (const firewall of firewalls) {
             await queryRunner.query(
-                "INSERT INTO system (firewall_cluster_id, node_type) VALUES (?, 'System')",
-                [item.id]
+                "INSERT INTO `fwc_tree` (`parent_id`, `name`, `node_type`) VALUES (?, 'System', 'SYS')",
+                [firewall.id]
+            );
+        }
+
+        const systems = await queryRunner.query(
+            "SELECT `id` FROM `fwc_tree` WHERE `name` = 'System'"
+        );
+
+        for (const system of systems) {
+            // Inserta nodo DHCP
+            await queryRunner.query(
+                "INSERT INTO `fwc_tree` (`parent_id`, `name`, `node_type`) VALUES (?, 'DHCP', 'SYS_CHILD')",
+                [system.id]
             );
 
-            // Retrieve the id of the newly created system entry
-            const [systemEntry] = await queryRunner.query(
-                "SELECT id FROM system WHERE firewall_cluster_id = ? AND node_type = 'System'",
-                [item.id]
+            // Inserta nodo Keepalived
+            await queryRunner.query(
+                "INSERT INTO `fwc_tree` (`parent_id`, `name`, `node_type`) VALUES (?, 'Keepalived', 'SYS_CHILD')",
+                [system.id]
             );
 
-            // Create child nodes: DHCP, Keepalived, and HAProxy for the system entry."
+            // Inserta nodo HAProxy
             await queryRunner.query(
-                "INSERT INTO system (firewall_cluster_id, node_type) VALUES (?, 'DHCP'), (?, 'Keepalived'), (?, 'HAProxy')",
-                [systemEntry.id, systemEntry.id, systemEntry.id]
+                "INSERT INTO `fwc_tree` (`parent_id`, `name`, `node_type`) VALUES (?, 'HAProxy', 'SYS_CHILD')",
+                [system.id]
             );
         }
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        // Elimina los nodos hijos de 'System'
+        await queryRunner.query(
+            "DELETE t1 FROM `fwc_tree` t1 JOIN `fwc_tree` t2 ON t1.`parent_id` = t2.`id` WHERE t2.`name` = 'System' AND t1.`name` IN ('DHCP', 'Keepalived', 'HAProxy')"
+        );
+
+        // Elimina los nodos 'System' que son hijos de nodos con tipo 'firewall'
+        await queryRunner.query(
+            "DELETE t1 FROM `fwc_tree` t1 JOIN `fwc_tree` t2 ON t1.`parent_id` = t2.`id` WHERE t2.`node_type` = 'firewall' AND t1.`name` = 'System'"
+        );
+
         await queryRunner.query(
             "DELETE FROM `fwc_tree_node_types` WHERE `node_type` = 'SYS' AND `name` = 'System'"
         );
 
-        await queryRunner.dropTable('system');
+        await queryRunner.query(
+            "DELETE FROM `fwc_tree_node_types` WHERE `node_type` = 'SYS' AND `name` = 'DHCP'"
+        );
+
+        await queryRunner.query(
+            "DELETE FROM `fwc_tree_node_types` WHERE `node_type` = 'SYS' AND `name` = 'Keepalived'"
+        );
+
+        await queryRunner.query(
+            "DELETE FROM `fwc_tree_node_types` WHERE `node_type` = 'SYS' AND `name` = 'HAProxy'"
+        );
     }
 }
