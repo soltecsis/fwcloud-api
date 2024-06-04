a7834b9e3944c0671a20905df7616bae09d50edc
aortega <aortega@soltecsis.com>
2024-06-04 16:01:48 +0200

    Modify config add more rules to reduce errors, first iteration

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,8 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking"
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended"
   ],
   root: true,
   env: {
@@ -16,15 +17,29 @@ module.exports = {
     es6: true,
   },
   rules: {
+    'prettier/prettier': 'error',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unsafe-call': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
-    "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/no-unnecessary-type-assertion": "off",
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/require-await': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    'no-control-regex': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    'no-async-promise-executor': 'off',
+    'no-prototype-builtins': 'off',
+    '@typescript-eslint/ban-types': 'off',
+
+
   },
   ignorePatterns: ['dist/', 'node_modules/', 'lib/'],
 };
-  
