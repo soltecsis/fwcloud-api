88e6b653d5455aa438b1a805db0ebee0f70cbee3
iortega <iortega@soltecsis.com>
2024-10-01 10:51:17 +0200

    Refactor migration to change 'plugins' column type from smallint to int using a temporary column

--- src/database/migrations/1.0.0/1716792622695-ChangePluginsTypeSmallintToInt.ts
+++ src/database/migrations/1.0.0/1716792622695-ChangePluginsTypeSmallintToInt.ts
@@ -1,53 +1,52 @@
 import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
 export class ChangePluginsTypeSmallintToInt1716792622695 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.changeColumn(
-      'firewall',
-      'plugins',
+  private async changeColumnType(
+    queryRunner: QueryRunner,
+    tableName: string,
+    oldType: string,
+    newType: string,
+  ): Promise<void> {
+    const tempColumnName = 'plugins_temp';
+
+    // Create a temporary column to store the data
+    await queryRunner.addColumn(
+      tableName,
       new TableColumn({
-        name: 'plugins',
-        type: 'int',
-        isNullable: false,
-        default: 0,
+        name: tempColumnName,
+        type: newType,
+        isNullable: true,
       }),
     );
 
+    // Copy the data from the original column to the temporary column.
+    await queryRunner.query(`UPDATE ${tableName} SET ${tempColumnName} = plugins;`);
+
+    // Delete the original column
+    await queryRunner.dropColumn(tableName, 'plugins');
+
+    // Rename the temporary column to plugins
+    await queryRunner.renameColumn(tableName, tempColumnName, 'plugins');
+
+    // Ensure that the column is not null
     await queryRunner.changeColumn(
-      'cluster',
+      tableName,
       'plugins',
       new TableColumn({
         name: 'plugins',
-        type: 'int',
+        type: newType,
         isNullable: false,
-        default: 0,
       }),
     );
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.changeColumn(
-      'firewall',
-      'plugins',
-      new TableColumn({
-        name: 'plugins',
-        type: 'smallint',
-        length: '2',
-        isNullable: false,
-        default: 0,
-      }),
-    );
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await this.changeColumnType(queryRunner, 'firewall', 'smallint', 'int');
+    await this.changeColumnType(queryRunner, 'cluster', 'smallint', 'int');
+  }
 
-    await queryRunner.changeColumn(
-      'cluster',
-      'plugins',
-      new TableColumn({
-        name: 'plugins',
-        type: 'smallint',
-        length: '2',
-        isNullable: false,
-        default: 0,
-      }),
-    );
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await this.changeColumnType(queryRunner, 'firewall', 'int', 'smallint');
+    await this.changeColumnType(queryRunner, 'cluster', 'int', 'smallint');
   }
 }
