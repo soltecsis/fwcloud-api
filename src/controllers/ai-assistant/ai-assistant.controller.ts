340f18687c7e32f4fabaeebb02837fce0fdc0ab6
iortega <iortega@soltecsis.com>
2024-12-04 15:33:39 +0100

    Refactor AI assistant controller to improve service integration and add configuration management methods

--- src/controllers/ai-assistant/ai-assistant.controller.ts
+++ src/controllers/ai-assistant/ai-assistant.controller.ts
@@ -1,17 +1,78 @@
 import { Request, Response } from 'express';
-import { Application } from '../../Application';
 import { Controller } from '../../fonaments/http/controller';
 import { ResponseBuilder } from '../../fonaments/http/response-builder';
 import { Validate } from '../../decorators/validate.decorator';
-import { AIassistantService } from '../../models/ai-assistant/ai-assistant.service';
+import { AIAssistantService } from '../../models/ai-assistant/ai-assistant.service';
 import { AiAssistantDto } from './dto/ai-assistant.dto';
+import { Firewall } from '../../models/firewall/Firewall';
+import { FwCloud } from '../../models/fwcloud/FwCloud';
+import db from '../../database/database-manager';
 
 export class AIassistantController extends Controller {
-  private _AIassistantService: AIassistantService;
+  private _aiAssistantService: AIAssistantService;
+  protected _firewall: Firewall;
+  protected _fwCloud: FwCloud;
 
-  constructor(app: Application) {
-    super(app);
-    this._AIassistantService = new AIassistantService(app);
+  public async make(req: Request): Promise<void> {
+    this._aiAssistantService = await this._app.getService<AIAssistantService>(
+      AIAssistantService.name,
+    );
+
+    if (req.params.firewall) {
+      this._firewall = await db
+        .getSource()
+        .manager.getRepository(Firewall)
+        .findOneOrFail({ where: { id: parseInt(req.params.firewall) } });
+    }
+    if (req.params.fwcloud) {
+      this._fwCloud = await db
+        .getSource()
+        .manager.getRepository(FwCloud)
+        .findOneOrFail({ where: { id: parseInt(req.params.fwcloud) } });
+    }
+  }
+
+  @Validate()
+  public async getConfig(req: Request, res: Response): Promise<ResponseBuilder> {
+    try {
+      const config = await this._aiAssistantService.getAiCredentials();
+
+      if (!config) {
+        return ResponseBuilder.buildResponse()
+          .status(404)
+          .body({ error: 'No AI assistant configuration found.' });
+      } else {
+        return ResponseBuilder.buildResponse().status(200).body(config);
+      }
+    } catch (error) {
+      return ResponseBuilder.buildResponse()
+        .status(500)
+        .body({ error: 'Failed to fetch AI assistant configuration.' });
+    }
+  }
+
+  @Validate(AiAssistantDto)
+  public async updateConfig(req: Request, res: Response): Promise<void> {
+    try {
+      const config = this._aiAssistantService.upateOrCreateAiCredentials(
+        req.body.ai,
+        req.body.model,
+        req.body.apiKey,
+      );
+      res.status(200).send(config);
+    } catch (error) {
+      res.status(500).send({ error: 'Failed to update AI assistant configuration.' });
+    }
+  }
+
+  @Validate(AiAssistantDto)
+  public async deleteConfig(req: Request, res: Response): Promise<void> {
+    try {
+      const config = this._aiAssistantService.deleteAiCredentials(req.body);
+      res.status(200).send(config);
+    } catch (error) {
+      res.status(500).send({ error: 'Failed to delete AI assistant configuration.' });
+    }
   }
 
   @Validate(AiAssistantDto)
@@ -23,12 +84,12 @@ export class AIassistantController extends Controller {
         return;
       }
 
-      const policyScript = await this._AIassistantService.getPolicyScript(
+      const policyScript = await this._aiAssistantService.getPolicyScript(
         parseInt(req.params.fwcloud),
         parseInt(req.params.firewall),
       );
 
-      const response = await this._AIassistantService.getResponse(prompt + '\n' + policyScript);
+      const response = await this._aiAssistantService.getResponse(prompt + '\n' + policyScript);
 
       console.log(response);
       return ResponseBuilder.buildResponse().status(200).body(response);
@@ -46,7 +107,7 @@ export class AIassistantController extends Controller {
         res.status(400).send({ error: 'Prompt text is required.' });
         return;
       }
-      const response = await this._AIassistantService.getResponse(prompt);
+      const response = await this._aiAssistantService.getResponse(prompt);
 
       res.status(200).send({ response });
     } catch (error) {
