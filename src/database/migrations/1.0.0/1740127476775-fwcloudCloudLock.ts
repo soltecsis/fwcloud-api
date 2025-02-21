0e9822eec7ad73b094b9ebe73feadc7395a433ae
aortega <aortega@soltecsis.com>
2025-02-21 12:45:43 +0100

    FIX: Delete unnecessary check code on migration

--- src/database/migrations/1.0.0/1740127476775-fwcloudCloudLock.ts
+++ src/database/migrations/1.0.0/1740127476775-fwcloudCloudLock.ts
@@ -7,33 +7,17 @@ export class FwcloudCloudLock1740127476775 implements MigrationInterface {
     await queryRunner.query(`UPDATE fwcloud SET locked_by = NULL, locked_at = NULL, locked = 0;`);
     await queryRunner.query(`ALTER TABLE fwcloud MODIFY COLUMN locked_by VARCHAR(255)`);
 
-    const result = await queryRunner.query('SHOW CREATE TABLE fwcloud');
-
-    // If index doesn't already exist, then create it.
-    if (
-      result.length === 1 &&
-      result[0]['Create Table'].search(FwcloudCloudLock1740127476775.idxLockedBy) === -1
-    ) {
-      await queryRunner.query(
-        `ALTER TABLE fwcloud ADD INDEX ${FwcloudCloudLock1740127476775.idxLockedBy} (locked_by)`,
-      );
-    }
+    await queryRunner.query(
+      `ALTER TABLE fwcloud ADD INDEX ${FwcloudCloudLock1740127476775.idxLockedBy} (locked_by)`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`UPDATE fwcloud SET locked_by = NULL, locked_at = NULL, locked = 0;`);
     await queryRunner.query('ALTER TABLE fwcloud MODIFY COLUMN locked_by INT;');
 
-    const result = await queryRunner.query('SHOW CREATE TABLE fwcloud');
-
-    // If index exists, then drop it.
-    if (
-      result.length === 1 &&
-      result[0]['Create Table'].search(FwcloudCloudLock1740127476775.idxLockedBy) !== -1
-    ) {
-      await queryRunner.query(
-        `ALTER TABLE fwcloud DROP INDEX ${FwcloudCloudLock1740127476775.idxLockedBy}`,
-      );
-    }
+    await queryRunner.query(
+      `ALTER TABLE fwcloud DROP INDEX ${FwcloudCloudLock1740127476775.idxLockedBy}`,
+    );
   }
 }
