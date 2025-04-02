17a16d87188253adc2aaf9647e3f7cae61cd0269
soltecsis-jfernandez <jfernandez@soltecsis.com>
2025-04-02 13:46:31 +0200

    ADD: added missing validations from wireguard config options

--- src/routes/vpn/wireguard/dto/store.dto.ts
+++ src/routes/vpn/wireguard/dto/store.dto.ts
@@ -8,8 +8,66 @@ import {
   IsIn,
   Matches,
   ValidateNested,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  Validate,
 } from 'class-validator';
+@ValidatorConstraint({ async: false })
+export class WireGuardOptionValidator implements ValidatorConstraintInterface {
+  validate(option: WireGuardOptionDTO) {
+    if (!option.arg) return true;
 
+    switch (option.name) {
+      // [Interface]
+      case 'PrivateKey':
+        return /^[A-Za-z0-9+/]{43}=$/.test(option.arg);
+      case 'Address':
+        return /^(\d{1,3}\.){3}\d{1,3}\/\d{1,2}(,\s*(\d{1,3}\.){3}\d{1,3}\/\d{1,2})*$/.test(
+          option.arg,
+        );
+      case 'ListenPort':
+        return /^([1-9]\d{0,3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5])$/.test(
+          option.arg,
+        );
+      case 'MTU':
+        return /^\d+$/.test(option.arg) && parseInt(option.arg) > 0;
+      case 'DNS':
+        return /^((\d{1,3}\.){3}\d{1,3}|[a-zA-Z0-9.-]+)(,\s*((\d{1,3}\.){3}\d{1,3}|[a-zA-Z0-9.-]+))*$/.test(
+          option.arg,
+        );
+      case 'Table':
+        return option.arg === 'off' || option.arg === '';
+
+      // [Peer]
+      case 'PublicKey':
+        return /^[A-Za-z0-9+/]{42,44}$/.test(option.arg);
+      case 'AllowedIPs':
+        return /^(\d{1,3}\.){3}\d{1,3}\/\d{1,2}(,\s*(\d{1,3}\.){3}\d{1,3}\/\d{1,2})*$/.test(
+          option.arg,
+        );
+      case 'Endpoint':
+        return /^((\d{1,3}\.){3}\d{1,3}|[a-zA-Z0-9.-]+):(\d{1,5})$/.test(option.arg);
+      case 'PersistentKeepalive':
+        return /^\d+$/.test(option.arg) && parseInt(option.arg) <= 65535;
+      case 'PresharedKey':
+        return /^[A-Za-z0-9+/]{43}=$/.test(option.arg);
+
+      // Commands
+      case 'PreUp':
+      case 'PostUp':
+      case 'PreDown':
+      case 'PostDown':
+        return option.arg.length > 0;
+
+      default:
+        return true;
+    }
+  }
+
+  defaultMessage() {
+    return 'Invalid option configuration for $property';
+  }
+}
 export class WireGuardOptionDTO {
   @IsString()
   @IsIn([
@@ -36,6 +94,7 @@ export class WireGuardOptionDTO {
 
   @IsOptional()
   @IsString()
+  @Validate(WireGuardOptionValidator)
   arg?: string;
 
   @IsOptional()
