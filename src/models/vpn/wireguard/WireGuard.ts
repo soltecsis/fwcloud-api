304db0f089a0f22aad621399faa2b741b63df47e
iortega <iortega@soltecsis.com>
2025-04-16 08:37:15 +0200

    UPDATE: Extend WireGuard options in SQL queries to include additional configuration parameters

--- src/models/vpn/wireguard/WireGuard.ts
+++ src/models/vpn/wireguard/WireGuard.ts
@@ -581,7 +581,7 @@ export class WireGuard extends Model {
         const sqlOpts = `SELECT *
                          FROM wireguard_opt OPT
                          WHERE OPT.wireguard=${wireGuard}
-                         AND OPT.name IN ('Address', 'ListenPort', 'DNS')
+                         AND OPT.name IN ('Address', 'ListenPort', 'DNS', 'MTU', 'Table', 'PreUp', 'PostUp', 'PreDown', 'PostDown', 'SaveConfig', 'FwMark')
                          ORDER BY OPT.order`;
         dbCon.query(sqlOpts, (optError, optResult) => {
           if (optError) return reject(optError);
@@ -601,12 +601,12 @@ export class WireGuard extends Model {
               ? `SELECT PEER.*, OPT.name option_name, OPT.arg option_value, OPT.comment option_comment
                  FROM wireguard_opt OPT
                  INNER JOIN wireguard PEER ON PEER.id=OPT.wireguard
-                 WHERE OPT.wireguard=${wireGuard} AND OPT.name IN ('Address', 'AllowedIPs', 'Endpoint', 'PersistentKeepalive', 'disable') AND OPT.wireguard_cli IS NULL
+                 WHERE OPT.wireguard=${wireGuard} AND OPT.name IN ('Address', 'AllowedIPs', 'Endpoint', 'PersistentKeepalive', 'PresharedKey', 'disable') AND OPT.wireguard_cli IS NULL
                  ORDER BY OPT.name`
               : `SELECT PEER.*, OPT.name option_name, OPT.arg option_value, OPT.comment option_comment
                  FROM wireguard PEER 
                  INNER JOIN wireguard_opt OPT ON OPT.wireguard=PEER.id 
-                 WHERE PEER.wireguard=${wireGuard} AND OPT.name IN ('Address', 'disable')
+                 WHERE PEER.wireguard=${wireGuard} AND OPT.name IN ('Address', 'AllowedIPs', 'disable')
                  ORDER BY OPT.name`;
 
             dbCon.query(sqlPeers, async (peerError, peerResult) => {
