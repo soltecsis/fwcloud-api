a7c5f1e71c17f4dec143b1dfe62a47496af171cd
iortega <iortega@soltecsis.com>
2025-05-13 13:09:37 +0200

    ADD: Implement WireGuard IP object retrieval tests for guest, regular, and admin users

--- tests/e2e/api/vpn/wireguard/wireguard.e2e.spec.ts
+++ tests/e2e/api/vpn/wireguard/wireguard.e2e.spec.ts
@@ -434,5 +434,58 @@ describe.only(describeName('WireGuard E2E Tests'), () => {
           });
       });
     });
+
+    describe('@getIpObj', async () => {
+      it('guest user should not be able to get WireGuard IP object', async () => {
+        await request(app.express)
+          .put(_URL().getURL('vpn.wireguard.ipobj.get'))
+          .send({
+            fwcloud: fwcProduct.fwcloud.id,
+            wireguard: fwcProduct.wireguardServer.id,
+          })
+          .then((response) => {
+            expect(response.status).to.equal(401);
+          });
+      });
+
+      it('regular user wich doest not belong to the fwcloud should not be able to get WireGuard IP object', async () => {
+        await request(app.express)
+          .put(_URL().getURL('vpn.wireguard.ipobj.get'))
+          .set('Cookie', [attachSession(loggedUserSessionId)])
+          .send({
+            fwcloud: 99999,
+            wireguard: fwcProduct.wireguardServer.id,
+          })
+          .then((response) => {
+            expect(response.status).to.equal(400);
+          });
+      });
+
+      it('regular user should be able to get WireGuard IP object', async () => {
+        await request(app.express)
+          .put(_URL().getURL('vpn.wireguard.ipobj.get'))
+          .set('Cookie', [attachSession(loggedUserSessionId)])
+          .send({
+            fwcloud: fwcProduct.fwcloud.id,
+            wireguard: fwcProduct.wireguardServer.id,
+          })
+          .then((response) => {
+            expect(response.status).to.equal(200);
+          });
+      });
+
+      it('admin user should be able to get WireGuard IP object', async () => {
+        await request(app.express)
+          .put(_URL().getURL('vpn.wireguard.ipobj.get'))
+          .set('Cookie', [attachSession(adminUserSessionId)])
+          .send({
+            fwcloud: fwcProduct.fwcloud.id,
+            wireguard: fwcProduct.wireguardServer.id,
+          })
+          .then((response) => {
+            expect(response.status).to.equal(200);
+          });
+      });
+    });
   });
 });
