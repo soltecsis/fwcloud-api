cca80b792de1a11767797aa2720f1e07d7bca05f
iortega <iortega@soltecsis.com>
2025-07-16 08:29:40 +0200

    FIX: Correct join references in IPSecPrefixService for accurate database queries

--- src/models/vpn/ipsec/ipsec-prefix.service.ts
+++ src/models/vpn/ipsec/ipsec-prefix.service.ts
@@ -56,7 +56,7 @@ export class IPSecPrefixService extends Service {
       .set({
         name: req.body.name,
       })
-      .where('node_type = :type', { type: 'PRI' }) //TODO: REVISAR PREFIX IPSEC
+      .where('node_type = :type', { type: 'PRI' })
       .andWhere('id_obj = :id', { id: req.body.prefix })
       .execute();
 
@@ -93,20 +93,20 @@ export class IPSecPrefixService extends Service {
 
           .leftJoin('rule.routingRuleToIPObjGroups', 'routingRuleToIPObjGroups')
           .leftJoin('routingRuleToIPObjGroups.ipObjGroup', 'ruleGroup')
-          .leftJoin('ruleGroupipSecPrefixes', 'ruleGroupIPSecPrefix')
+          .leftJoin('ruleGroup.ipSecPrefixes', 'ruleGroupIPSecPrefix')
 
           .leftJoin('rule.routingRuleToIPSecPrefixes', 'routingRuleToIPSecPrefix')
 
           .leftJoin('route.routeToIPObjGroups', 'routeToIPObjGroups')
           .leftJoin('routeToIPObjGroups.ipObjGroup', 'routeGroup')
-          .leftJoin('routeGroupipSecPrefixes', 'routeGroupIPSecPrefix')
+          .leftJoin('routeGroup.ipSecPrefixes', 'routeGroupIPSecPrefix')
 
           .leftJoin('route.routeToIPSecPrefixes', 'routeToIPSecPrefix')
 
-          .where('routingRuleToIPSecPrefixipSecPrefixId = :idRoutingRule', {
+          .where('routingRuleToIPSecPrefix.ipSecPrefixId = :idRoutingRule', {
             idRoutingRule: prefixId,
           })
-          .orWhere('routeToIPSecPrefixipSecPrefixId = :idRoute', { idRoute: prefixId })
+          .orWhere('routeToIPSecPrefix.ipSecPrefixId = :idRoute', { idRoute: prefixId })
           .orWhere('ruleGroupIPSecPrefix.id = :idRuleGroupPrefix', {
             idRuleGroupPrefix: prefixId,
           })
