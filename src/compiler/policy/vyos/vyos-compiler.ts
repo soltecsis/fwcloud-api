d8a01c50fd11813611faa26c507d59795562918a
iortega <iortega@soltecsis.com>
2025-09-10 09:08:44 +0200

    ADD: enhance VyOSCompiler to support multiple interfaces and geoip country codes

--- src/compiler/policy/vyos/vyos-compiler.ts
+++ src/compiler/policy/vyos/vyos-compiler.ts
@@ -21,6 +21,8 @@
 
 import { PolicyCompilerTools, POLICY_TYPE } from '../PolicyCompilerTools';
 import { PolicyTypesMap } from '../../../models/policy/PolicyType';
+import { FireWallOptMask } from '../../../models/firewall/Firewall';
+import { PolicyRuleOptMask } from '../../../models/policy/PolicyRule';
 import { IpUtils } from '../../../utils/ip-utils';
 
 export class VyOSCompiler extends PolicyCompilerTools {
@@ -64,16 +66,20 @@ export class VyOSCompiler extends PolicyCompilerTools {
     const base = (opt: string) => `set firewall name ${chain} rule ${this._ruleData.id} ${opt}`;
 
     // Interface IN
-    const inIf = this._ruleData.positions?.[0]?.ipobjs?.[0]?.name;
-    if (inIf) this._cs += `${base('inbound-interface')} ${inIf}\n`;
+    const inIfs = this._ruleData.positions?.[0]?.ipobjs || [];
+    for (const iface of inIfs) {
+      if (iface?.name) this._cs += `${base('inbound-interface')} ${iface.name}\n`;
+    }
 
     // Interface OUT for FORWARD rules
     if (
       this._policyType === PolicyTypesMap.get('IPv4:FORWARD') ||
       this._policyType === PolicyTypesMap.get('IPv6:FORWARD')
     ) {
-      const outIf = this._ruleData.positions?.[1]?.ipobjs?.[0]?.name;
-      if (outIf) this._cs += `${base('outbound-interface')} ${outIf}\n`;
+      const outIfs = this._ruleData.positions?.[1]?.ipobjs || [];
+      for (const iface of outIfs) {
+        if (iface?.name) this._cs += `${base('outbound-interface')} ${iface.name}\n`;
+      }
     }
 
     // Determine positions
@@ -85,19 +91,41 @@ export class VyOSCompiler extends PolicyCompilerTools {
     const svcPos = forward ? 4 : 3;
 
     // Source
-    const srcAddr = this.formatAddress(this._ruleData.positions?.[srcPos]?.ipobjs?.[0]);
-    if (srcAddr) this._cs += `${base('source address')} ${srcAddr}\n`;
+    const srcObjs = this._ruleData.positions?.[srcPos]?.ipobjs || [];
+    for (const obj of srcObjs) {
+      if (obj?.type === 24) {
+        const cc = obj.name || obj.code;
+        if (cc) this._cs += `${base('source geoip country-code')} ${cc}\n`;
+        continue;
+      }
+      const srcAddr = this.formatAddress(obj);
+      if (srcAddr) this._cs += `${base('source address')} ${srcAddr}\n`;
+    }
 
     // Destination
-    const dstAddr = this.formatAddress(this._ruleData.positions?.[dstPos]?.ipobjs?.[0]);
-    if (dstAddr) this._cs += `${base('destination address')} ${dstAddr}\n`;
+    const dstObjs = this._ruleData.positions?.[dstPos]?.ipobjs || [];
+    for (const obj of dstObjs) {
+      if (obj?.type === 24) {
+        const cc = obj.name || obj.code;
+        if (cc) this._cs += `${base('destination geoip country-code')} ${cc}\n`;
+        continue;
+      }
+      const dstAddr = this.formatAddress(obj);
+      if (dstAddr) this._cs += `${base('destination address')} ${dstAddr}\n`;
+    }
 
     // Service / protocol
-    const svc = this._ruleData.positions?.[svcPos]?.ipobjs?.[0];
-    if (svc) {
+    const svcObjs = this._ruleData.positions?.[svcPos]?.ipobjs || [];
+    for (const svc of svcObjs) {
       const proto = svc.protocol;
       const protoName = proto === 6 ? 'tcp' : proto === 17 ? 'udp' : proto === 1 ? 'icmp' : proto;
       this._cs += `${base('protocol')} ${protoName}\n`;
+      if (proto === 1) {
+        if (svc.icmp_type !== undefined && svc.icmp_type !== -1)
+          this._cs += `${base('icmp type')} ${svc.icmp_type}\n`;
+        if (svc.icmp_code !== undefined && svc.icmp_code !== -1)
+          this._cs += `${base('icmp code')} ${svc.icmp_code}\n`;
+      }
       if (svc.source_port_end && svc.source_port_end !== 0) {
         const srange =
           svc.source_port_start === svc.source_port_end
@@ -112,6 +140,20 @@ export class VyOSCompiler extends PolicyCompilerTools {
             : `${svc.destination_port_start}-${svc.destination_port_end}`;
         this._cs += `${base('destination port')} ${drange}\n`;
       }
+    }
+
+    // Connection state
+    const needsState =
+      this._ruleData.action === 1 &&
+      (this._ruleData.options & PolicyRuleOptMask.STATEFUL ||
+        (this._ruleData.firewall_options & FireWallOptMask.STATEFUL &&
+          !(this._ruleData.options & PolicyRuleOptMask.STATELESS)));
+    if (needsState) this._cs += `${base('state new')} enable\n`;
+
+    // Packet mark
+    const markCode = parseInt(this._ruleData.mark_code);
+    if (markCode && this._policyType <= PolicyTypesMap.get('IPv4:FORWARD')) {
+      this._cs += `${base('set-mark')} ${markCode}\n`;
     }
 
     // Action
