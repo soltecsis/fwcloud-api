e15ae04d33de3cca552fb546c6577ab7c8628e06
soltecsis-mseva <marcos.s.berenguer@gmail.com>
2025-09-15 11:30:09 +0200

    FEAT: add safety check for dangerous rules during policy compilation and emit progress notifications (dangerous rules list still hardcoded, pending DB integration)

--- src/compiler/policy/PolicyCompiler.ts
+++ src/compiler/policy/PolicyCompiler.ts
@@ -24,13 +24,26 @@ import { IPTablesCompiler } from './iptables/iptables-compiler';
 import { NFTablesCompiler } from './nftables/nftables-compiler';
 import { RuleCompilationResult } from './PolicyCompilerTools';
 import { EventEmitter } from 'typeorm/platform/PlatformTools';
-import { ProgressNoticePayload } from '../../sockets/messages/socket-message';
+import {
+  ProgressNoticePayload,
+  ProgressWarningPayload,
+} from '../../sockets/messages/socket-message';
 import { PolicyRule } from '../../models/policy/PolicyRule';
 
 export type PolicyCompilerClasses = IPTablesCompiler | NFTablesCompiler;
 export type AvailablePolicyCompilers = 'IPTables' | 'NFTables';
 
 export class PolicyCompiler {
+  private static checkRuleSafety(rule: string, dangerousRules: any[]): boolean {
+    let processedRule = rule;
+    if (processedRule.endsWith('\n')) {
+      processedRule = processedRule.slice(0, -1);
+    }
+    // Remove '-m comment --comment "string_comment"' from the rule if present
+    processedRule = processedRule.replace(/-m comment --comment\s+'[^']*'\s*/g, '');
+    return dangerousRules.includes(processedRule);
+  }
+
   public static compile(
     compileFor: AvailablePolicyCompilers,
     rulesData: any,
@@ -42,27 +55,46 @@ export class PolicyCompiler {
 
         if (!rulesData) return resolve(result);
 
-        for (let i = 0; i < rulesData.length; i++) {
-          if (eventEmitter)
-            eventEmitter.emit(
-              'message',
-              new ProgressNoticePayload(
-                `Rule ${i + 1} (ID: ${rulesData[i].id})${!rulesData[i].active ? ' [DISABLED]' : ''}`,
-              ),
-            );
+        // TODO: Move list to database and load it from there
+        const dangerousRules: string[] = [
+          '$IPTABLES -A INPUT -m conntrack --ctstate NEW -j ACCEPT',
+          '$IPTABLES -A FORWARD -m conntrack --ctstate NEW -j ACCEPT',
+        ];
 
+        for (let i = 0; i < rulesData.length; i++) {
+          let dangerous = false;
           let compiler: PolicyCompilerClasses;
 
           if (compileFor == 'IPTables') compiler = new IPTablesCompiler(rulesData[i]);
           // NFTables
           else compiler = new NFTablesCompiler(rulesData[i]);
 
+          const compiledRule = compiler.ruleCompile();
+
           result.push({
             id: rulesData[i].id,
             active: rulesData[i].active,
             comment: rulesData[i].comment,
-            cs: rulesData[i].active || rulesData.length === 1 ? compiler.ruleCompile() : '',
+            cs: rulesData[i].active || rulesData.length === 1 ? compiledRule : '',
           });
+
+          if (eventEmitter) {
+            // Check if rule is dangerous
+            dangerous = this.checkRuleSafety(compiledRule, dangerousRules);
+
+            if (!dangerous)
+              eventEmitter.emit(
+                'message',
+                new ProgressNoticePayload(
+                  `Rule ${i + 1} (ID: ${rulesData[i].id})${!rulesData[i].active ? ' [DISABLED]' : ''}`,
+                ),
+              );
+            else
+              eventEmitter.emit(
+                'message',
+                new ProgressWarningPayload(`Rule ${i + 1} (ID: ${rulesData[i].id}) [DANGEROUS]`),
+              );
+          }
         }
 
         resolve(result);
