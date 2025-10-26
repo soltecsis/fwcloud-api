50b29bf25b7e290448c521e03b54d53a98607570
soltecsis-mseva <marcos.s.berenguer@gmail.com>
2025-10-26 11:11:57 +0100

    UPDATE: Normalize casing for IPv4 and IPv6 in dangerousRules.ts and dangerous-rules.json

--- src/config/policy/dangerousRules.ts
+++ src/config/policy/dangerousRules.ts
@@ -2,20 +2,20 @@ import dangerousRules from './dangerous-rules.json';
 
 // Mapping of type to ipType and chain
 const typeMap = {
-  1: { ipType: 'ipv4', chain: 'INPUT' },
-  2: { ipType: 'ipv4', chain: 'OUTPUT' },
-  3: { ipType: 'ipv4', chain: 'FORWARD' },
-  4: { ipType: 'ipv4', chain: 'SNAT' },
-  5: { ipType: 'ipv4', chain: 'DNAT' },
-  61: { ipType: 'ipv6', chain: 'INPUT' },
-  62: { ipType: 'ipv6', chain: 'OUTPUT' },
-  63: { ipType: 'ipv6', chain: 'FORWARD' },
-  64: { ipType: 'ipv6', chain: 'SNAT' },
-  65: { ipType: 'ipv6', chain: 'DNAT' },
+  1: { ipType: 'IPv4', chain: 'INPUT' },
+  2: { ipType: 'IPv4', chain: 'OUTPUT' },
+  3: { ipType: 'IPv4', chain: 'FORWARD' },
+  4: { ipType: 'IPv4', chain: 'SNAT' },
+  5: { ipType: 'IPv4', chain: 'DNAT' },
+  61: { ipType: 'IPv6', chain: 'INPUT' },
+  62: { ipType: 'IPv6', chain: 'OUTPUT' },
+  63: { ipType: 'IPv6', chain: 'FORWARD' },
+  64: { ipType: 'IPv6', chain: 'SNAT' },
+  65: { ipType: 'IPv6', chain: 'DNAT' },
 };
 
 type DangerousRuleInfo = {
-  ruleIPType: 'ipv4' | 'ipv6';
+  ruleIPType: 'IPv4' | 'IPv6';
   ruleChainType: 'INPUT' | 'OUTPUT' | 'FORWARD' | 'SNAT' | 'DNAT';
   ruleOrder: number;
 };
